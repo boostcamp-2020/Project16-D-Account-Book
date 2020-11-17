716f55f8dfa40d4c7d229252f5e2af2ba5f2f00e
mu1616 <mu1616@naver.com>
2020-11-17 17:06:43 +0900

    [chore]: 백엔드 eslint, prettier 설정
    server 폴더(백엔드) 에서 eslint와 prettier 설정을 하였습니다.

---
+++ server/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    semi: "error",
+    "no-console": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "no-unused-vars": "warn",
+  },
+};
