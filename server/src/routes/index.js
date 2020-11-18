8f4efc2c8843c849343cce66be7fa0116b690b40
mu1616 <mu1616@naver.com>
2020-11-19 08:19:45 +0900

    chore: router 샘플코드 주석 추가

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,11 +2,11 @@ const Router = require('koa-router');
 
 const router = new Router();
 
-router.post('hello', (ctx, next) => {
-  console.log(ctx.request.body);
+// TODO: 라우터 사용법을 보여주기 위한 임시 코드 입니다. 추후 지워야할 코드입니다.
+router.get('hello', (ctx, next) => {
   const data = { name: 'PEPSI', price: 3000 };
   ctx.body = data;
-  throw new Error('에러 테스트');
+  // throw new Error('에러 테스트');
 });
 
 module.exports = router;
