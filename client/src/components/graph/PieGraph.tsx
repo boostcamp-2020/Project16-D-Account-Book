2bcf92fbceff00698908581a5d71db2941751f58
lacomaco <lacomaco122@gmail.com>
2020-11-22 22:12:35 +0900

    feature : #17 PieGraph 컴포넌트 생성
    - 카테고리  정보를 바탕으로 전체 파이그래프를 생성하는 컴포넌트를
      만들었습니다.
    
    - 우선은 카테고리 정보가 { title:카테고리 이름, color: 카테고리의
      색(그래프에 그릴),ratio : 카테고리가 차지하는 전체 비중 }의 형식으로
      들어온다고 가정하고 만들어 두었습니다.
    
    - 카테고리 색과 비중을 바탕으로 파이 그래프를 그리는 역할을 담당합니다.

--- client/src/components/graph/PieGraph.tsx
+++ client/src/components/graph/PieGraph.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import CircleSVG from './Circle';
+import styled from 'styled-components';
 
-function PieGraph() {
+const SVG = styled.svg`
+transform:rotate(-90deg);
+`;
 
+const SVGWrapper = styled.div`
+:after{
+  content:"";
+  display:block;
+  width:50%;
+  margin:0 auto;
+  height:2px;
+  background:rgba(0,0,0,0.1);
+  border-radius:50%;
+  box-shadow: 0 0 3px 4px rgba(0,0,0,0.1);
+}
+`;
+
+interface PieDrawInfo {
+  title: string;
+  color: string;
+  ratio: number;
 }
 
-export default PieGraph;
+interface PieGraphProps {
+  transactionData: PieDrawInfo[];
+}
+
+const PieGraphSVG = ({ transactionData }: PieGraphProps) => {
+  const svgViewBox = 100;
+
+  const PieGraphs = useMemo(() => {
+    let accumulate = 0;
+    return transactionData.map(transactionData => {
+      accumulate += transactionData.ratio;
+      return <CircleSVG
+        key={transactionData.title}
+        color={transactionData.color}
+        myPercent={transactionData.ratio}
+        beforePercent={accumulate - transactionData.ratio}
+        viewBox={svgViewBox}
+      />
+    });
+  }, [transactionData]);
+
+  return (
+    <SVGWrapper>
+      <SVG viewBox={`0 0 ${svgViewBox} ${svgViewBox}`}>
+        {PieGraphs}
+      </SVG >
+    </SVGWrapper>
+  )
+}
+
+export default PieGraphSVG;
