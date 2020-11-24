872e90d5ec77f2a656685429905d0cefbb399eb9
Mong-Gu <yhw8258@naver.com>
2020-11-24 18:09:23 +0900

    feat: oauth 관련 config 추가

---
+++ server/src/config/oauth.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+
+const envFound = dotenv.config();
+
+if (envFound.error) {
+  throw new Error(`Could't find .env file `);
+}
+
+module.exports = {
+  naverClientId: process.env.NAVER_CLIENT_ID,
+  naverClientSecret: process.env.NAVER_CLIENT_SECRET,
+  naverCallbackUrl: process.env.NAVER_CALLBACK_URL,
+  naverState: process.env.NAVER_STATE,
+  naverUserToken: 'https://nid.naver.com/oauth2.0/token',
+  naverUserInfoURL: 'https://openapi.naver.com/v1/nid/me',
+  jwtSecretKey: process.env.JWT_SECRET_KEY,
+};
