302989ecab9fdd35c698c16bf7930ffc0e2d8ebe
lacomaco <lacomaco122@gmail.com>
2020-11-25 00:19:40 +0900

    feat : #87 modalBackground 컴포넌트 생성
    - modalBackground 컴포넌트는 children을 modal의 정중앙에 그려주는
      컴포넌트입니다.
    
    - props로 show 상태와 모달을 닫는 closeModal 함수를 받습니다.
    
    - modalBackground를 사용하기 위해서는 modal 상태를 관리하는 container
      컴포넌트로 감싸준후 사용해야합니다.

---
+++ client/src/components/modal-background/ModalBackground.tsx
@@ -0,0 +1,41 @@
+import React, { useRef, useState } from 'react';
+import useClickMe from '../../hook/use-click-me/useClickMe';
+import styled from 'styled-components';
+interface ModalBackgroundProps {
+  children?: React.ReactNode;
+  show: boolean;
+  closeModal: () => void;
+}
+
+interface ModalWrapperProps {
+  show: boolean;
+}
+
+const ModalBackgroundWrapper = styled.div<ModalWrapperProps>`
+  background-color: rgba(0, 0, 0, 0.4);
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0%;
+  left: 0%;
+  display: ${({ show }) => (show ? 'block' : 'none')};
+`;
+
+const ChildrenWrapper = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const ModalBackground: React.FC<ModalBackgroundProps> = ({ children, show, closeModal }: ModalBackgroundProps) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+  useClickMe(modalRef, closeModal);
+  return (
+    <ModalBackgroundWrapper ref={modalRef} show={show}>
+      <ChildrenWrapper>{children}</ChildrenWrapper>
+    </ModalBackgroundWrapper>
+  );
+};
+
+export default ModalBackground;
