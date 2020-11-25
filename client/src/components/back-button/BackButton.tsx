0d2f97d7d056bd68c983a5ec2154e547bbfcfcd7
lacomaco <lacomaco122@gmail.com>
2020-11-25 14:35:20 +0900

    feat : #94 뒤로가기 버튼 컴포넌트 flatIcon
    - flatIcon에서 주워온 < svg 컴포넌트를 추가하였습니다.

--- client/src/components/back-button/BackButton.tsx
+++ client/src/components/back-button/BackButton.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
-interface BackButton{
-  onClick?:()=>void;
+interface BackButton {
+  onClick?: () => void;
 }
 
-const BackButton:React.FC<BackButton> = ({onClick}:BackButton)=>{
+const BackButton: React.FC<BackButton> = ({ onClick }: BackButton) => {
   return (
-  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="32" height="32" viewBox="0 0 32 32" fill="#000000" data-svg-content="true">
-    <g>
-      <path d="M 4.292,15.708l 6,6c 0.39,0.39, 1.024,0.39, 1.414,0c 0.39-0.39, 0.39-1.024,0-1.414L 7.414,16L 22,16 c 4,0, 6,3.692, 6,7c0,0.552, 0.448,1, 1,1s 1-0.448, 1-1C 30,18.588, 28,14, 22,14L 7.414,14 l 4.292-4.292 c 0.39-0.39, 0.39-1.024,0-1.414c-0.39-0.39-1.024-0.39-1.414,0l-6,6C 3.902,14.684, 3.902,15.316, 4.292,15.708z"></path>
-    </g>
-    </svg>)
-}
+    <svg x="0px" y="0px" viewBox="0 0 306 306" onClick={onClick}>
+      <g>
+        <g id="chevron-left">
+          <polygon points="247.35,35.7 211.65,0 58.65,153 211.65,306 247.35,270.3 130.05,153   " />
+        </g>
+      </g>
+    </svg>
+  );
+};
 
 export default BackButton;
