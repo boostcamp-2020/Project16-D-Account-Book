ff199bfe866e52f882271c56bcf98da4b97916cc
mu1616 <mu1616@naver.com>
2020-11-25 09:24:57 +0900

    refactor: Category 컴포넌트 크기 변경
    반응형을 위해 % 로 크기를 수정하였습니다.

--- client/src/components/category/Category.tsx
+++ client/src/components/category/Category.tsx
@@ -3,12 +3,15 @@ import Styled from 'styled-components';
 import { getTextColor } from '../../utils/color';
 
 const CategoryWrapper = Styled.div<{ bgColor: string; textColor: string }>`
-  width: 115px;
+  width: 100%;
+  max-width: 115px;
+  height: 100%;
+  max-height: 50px;
   background-color: ${({ bgColor }) => bgColor};
   color: ${({ textColor }) => textColor};
   box-sizing: border-box;
   text-align: center;
-  padding: 7px 10px 7px 10px;
+  padding: 12px 10px 12px 10px;
   border-radius: 360px;
   font-weight: 500;
   white-space: nowrap;
