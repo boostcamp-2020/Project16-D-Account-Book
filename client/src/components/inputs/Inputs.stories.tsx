751568c3ec10ab5d9783ac19bd836baf92564327
lacomaco <lacomaco122@gmail.com>
2020-11-25 17:40:39 +0900

    feat : #69 드랍다운 컴포넌트 stories 구현
    - default 크기의 드랍다운 컴포넌트와
    - 작은 버전의 드랍다운 컴포넌트 2가지를 추가하였습니다.

--- client/src/components/inputs/Inputs.stories.tsx
+++ client/src/components/inputs/Inputs.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import InputText from './InputText';
-
+import Select, { Options } from './Select';
+import dummyOptions from '../../__dummy-data__/components/inputs/dummyOptions';
 export default {
   title: 'Inputs',
 };
@@ -11,4 +12,20 @@ export const InputTextDefault: React.FC = () => {
     //pass
   };
   return <InputText placeholder={'기본값입니다.'} value={value} onChange={onChange} />;
+};
+
+export const SelectDefault: React.FC = () => {
+  return <Select options={dummyOptions} defaultValue={'스토리북'} />;
+};
+
+export const SmallSelect: React.FC = () => {
+  return (
+    <div
+      style={{
+        width: '400px',
+      }}
+    >
+      <Select options={dummyOptions} defaultValue={'스토리북'} />
+    </div>
+  );
 };
