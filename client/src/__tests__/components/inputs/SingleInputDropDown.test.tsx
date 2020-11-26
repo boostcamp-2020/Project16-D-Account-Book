3df6a0fda2b17f518457b077e411960713464a15
lacomaco <lacomaco122@gmail.com>
2020-11-26 16:08:28 +0900

    refactor : DummyOption 데이터가 변경되어서 테스트 코드도 수정하엿습니다.

--- client/src/__tests__/components/inputs/SingleInputDropDown.test.tsx
+++ client/src/__tests__/components/inputs/SingleInputDropDown.test.tsx
@@ -14,7 +14,7 @@ describe('싱글 인풋 DropDown 테스트', () => {
   test('선택된것이 없다면 placeholder가 출력된다.', () => {
     screen.getByText(placeholder);
   });
-  test('아이템 선택시 해당 아이템이 header에 출력된다.', () => {
+  test('아이템 선택시 해당 아이템의 Label이 header에 출력된다.', () => {
     userEvent.click(screen.getByText(placeholder));
     userEvent.click(screen.getByText(/optionLabel1/));
     screen.getByText(/optionLabel1/);
@@ -22,6 +22,6 @@ describe('싱글 인풋 DropDown 테스트', () => {
   test('성공적으로 onChange 함수가 호출된다.', () => {
     userEvent.click(screen.getByText(placeholder));
     userEvent.click(screen.getByText(/optionLabel1/));
-    expect(spy.mock.calls[0][0]).toBe('optionLabel1');
+    expect(spy.mock.calls[0][0]).toBe('optionValue1');
   });
 });
