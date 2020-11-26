bcf77711313dbdc69a679c0654a5c64841aca883
mu1616 <mu1616@naver.com>
2020-11-27 07:31:12 +0900

    feat: 월의 말일인지 판단하는 유틸함수 구현
    - 해당 월의 말일인지 판단하여 true/false를 리턴하는 함수를
    구현하였습니다.

--- client/src/utils/date.ts
+++ client/src/utils/date.ts
@@ -2,3 +2,16 @@
 export const isLeapYear = (year: number): boolean => {
   return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
 };
+
+export const isEndOfMonth = (year: number, month: number, day: number): boolean => {
+  const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (month != 2) {
+    return day == monthDays[month - 1];
+  }
+
+  if (isLeapYear(year)) {
+    return day == 29;
+  }
+
+  return day == 28;
+};
