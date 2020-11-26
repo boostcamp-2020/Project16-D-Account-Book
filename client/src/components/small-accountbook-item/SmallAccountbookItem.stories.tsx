391e692fb4f610b8e1156d7a5888088ddc1e46bc
bbbyung2 <bhko0524@naver.com>
2020-11-27 06:11:10 +0900

    refactor: SmallAccountbookItem 컴포넌트 props 변경 및 stories 파일 title 변경
    - props를 text에서 id로 변경하고 인자로 들어가는 string을 number 형식으로 변경
    - SmallAccountbookItem.stories.tsx의 title 형식에 맞게 small-accountbook-item/SmallAccountbookItem으로 변경

--- client/src/components/small-accountbook-item/SmallAccountbookItem.stories.tsx
+++ client/src/components/small-accountbook-item/SmallAccountbookItem.stories.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import SmallAccountbookItem from './SmallAccountbookItem';
 
 export default {
-  title: '작은 가계부 아이템',
+  title: 'small-accountbook-item/SmallAccountbookItem',
   component: SmallAccountbookItem,
 };
 
 export const FirstSmallAccountbookItem = (): JSX.Element => {
-  return <SmallAccountbookItem text={'1'} bgColor={'#3498DB'} />;
+  return <SmallAccountbookItem id={1} bgColor={'#3498DB'} />;
 };
 
 export const SecondSmallAccountbookItem = (): JSX.Element => {
-  return <SmallAccountbookItem text={'2'} bgColor={'#2ecc70'} />;
+  return <SmallAccountbookItem id={2} bgColor={'#2ecc70'} />;
 };
 
 export const ThirdSmallAccountbookItem = (): JSX.Element => {
-  return <SmallAccountbookItem text={'3'} bgColor={'#f1c40f'} />;
+  return <SmallAccountbookItem id={3} bgColor={'#f1c40f'} />;
 };
