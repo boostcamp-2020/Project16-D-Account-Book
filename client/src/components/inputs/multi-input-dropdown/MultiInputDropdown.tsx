9367a1041ee49b873777414b6ba0f006aaf613f5
lacomaco <lacomaco122@gmail.com>
2020-11-26 17:21:03 +0900

    feat : MultiInputDropdown 컴포넌트를 HOC로 변경
    - 카드 선택, 일반 다중 dropdown 선택은 이름을 출력하는 로직만 다르기
      때문에 HOC로 변경하여 해당 로직을 함수로 받아 취사선택하도록
      수정하였습니다.

--- client/src/components/inputs/multi-input-dropdown/MultiInputDropdown.tsx
+++ client/src/components/inputs/multi-input-dropdown/MultiInputDropdown.tsx
@@ -1,43 +1,9 @@
-import React, { useState } from 'react';
 import { Options } from '../select/Select';
-import InputDropDown from '../input-drop-down/InputDropDown';
-import { setImmutable } from '../../../utils/immutable/setImmutable';
-interface Props {
-  placeholder: string;
-  items: Options[];
-  onChange?: (selected: string[]) => void;
-}
+import MultiInputDropDownHOC from './MultiInputHOC';
 
 const joinWithComma = (set: Set<string>, items: Options[]): string => {
-  const filterItem = items.filter((curr) => set.has(curr.value));
+  const filterItem = items.filter((curr) => set.has(curr.value as string));
   return filterItem.map((item) => item.label).join(',');
 };
 
-const MultiInputDropDown: React.FC<Props> = ({ placeholder, items, onChange }: Props) => {
-  const [selected, setSelected] = useState<Set<string>>(new Set<string>());
-
-  const selectChange = (value: string): void => {
-    if (selected.has(value)) {
-      selected.delete(value);
-    } else {
-      selected.add(value);
-    }
-    setSelected(setImmutable(selected));
-    if (onChange !== undefined) {
-      onChange(Array.from(selected));
-    }
-  };
-  const displayHeader = selected.size === 0 ? placeholder : joinWithComma(selected, items);
-  const selectedValue = Array.from(selected);
-  return (
-    <InputDropDown
-      items={items}
-      selectValue={selectedValue}
-      header={displayHeader}
-      onChange={selectChange}
-      multi={true}
-    />
-  );
-};
-
-export default MultiInputDropDown;
+export default MultiInputDropDownHOC(joinWithComma);
