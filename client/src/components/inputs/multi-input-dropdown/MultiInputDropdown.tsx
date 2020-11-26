6a7b32075f10cc0be505fedc9b6df74d1810e3ba
lacomaco <lacomaco122@gmail.com>
2020-11-26 16:09:29 +0900

    feat : #69 MultiInputDropDown 컴포넌트 구현
    - 드랍다운 아이템을 여러개 클릭할 수 있는 컴포넌트를 구체화시켰습니다.

--- client/src/components/inputs/multi-input-dropdown/MultiInputDropdown.tsx
+++ client/src/components/inputs/multi-input-dropdown/MultiInputDropdown.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Options } from '../select/Select';
+import InputDropDown from '../input-drop-down/InputDropDown';
+import { setImmutable } from '../../../utils/immutable/setImmutable';
 interface Props {
   placeholder: string;
+  items: Options[];
+  onChange?: (selected: string[]) => void;
 }
 
-const MultiInputDropDown: React.FC<Props> = ({ placeholder }: Props) => {
-  return <div>{placeholder}</div>;
+const joinWithComma = (set: Set<string>, items: Options[]): string => {
+  return items.reduce((acc, curr) => {
+    if (set.has(curr.value)) {
+      return acc + ',' + curr.label;
+    }
+    return acc + '';
+  }, '');
+};
+
+const MultiInputDropDown: React.FC<Props> = ({ placeholder, items, onChange }: Props) => {
+  const [selected, setSelected] = useState<Set<string>>(new Set<string>());
+
+  const selectChange = (value: string): void => {
+    if (selected.has(value)) {
+      selected.delete(value);
+    } else {
+      selected.add(value);
+    }
+    setSelected(setImmutable(selected));
+    if (onChange !== undefined) {
+      onChange(Array.from(selected));
+    }
+  };
+  const displayHeader = selected.size === 0 ? placeholder : joinWithComma(selected, items);
+  return <InputDropDown items={items} header={displayHeader} onChange={selectChange} multi={true} />;
 };
 
 export default MultiInputDropDown;
