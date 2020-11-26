7d1410f474b0ed4c0962ba5afd95d49403eb83a1
lacomaco <lacomaco122@gmail.com>
2020-11-26 16:30:30 +0900

    refactor : 가짜로 채워넣은 singleDropDown과 multiInputDropDown을 제거하였습니다.
    - 실제로 구현한 MultiInputDropDown 컴포넌트와 SingleInputDropDown
      컴포넌트를 채워넣었습니다.

--- client/src/components/inputs/Inputs.stories.tsx
+++ client/src/components/inputs/Inputs.stories.tsx
@@ -4,8 +4,8 @@ import Select, { Options } from './select/Select';
 import InputDropDown from './input-drop-down/InputDropDown';
 import styled from 'styled-components';
 import dummyOptions from '../../__dummy-data__/components/inputs/dummyOptions';
-import { stat } from 'fs';
 import MultiInputDropDown from './multi-input-dropdown/MultiInputDropdown';
+import SingleInputDropDown from './single-input-dropdown/SingleInputDropdown';
 
 const SmallDiv = styled.div`
   width: 300px;
@@ -22,53 +22,10 @@ export const InputTextDefault: React.FC = () => {
   return <InputText placeholder={'기본값입니다.'} value={value} onChange={onChange} />;
 };
 
-export const SmallInputDropDownSingle: React.FC = () => {
+export const InputDropDownDefault: React.FC = () => {
   return (
     <SmallDiv>
-      <InputDropDown items={dummyOptions} selectValue={['optionLabel1', 'optionLabel5']} header="스토리북" />
-    </SmallDiv>
-  );
-};
-
-export const SmallInputDropDownMulti: React.FC = () => {
-  return (
-    <SmallDiv>
-      <InputDropDown
-        items={dummyOptions}
-        selectValue={['optionLabel1', 'optionLabel5']}
-        header="스토리북"
-        multi={true}
-      />
-    </SmallDiv>
-  );
-};
-
-export const InputDropDownSingleUsage: React.FC = () => {
-  const [select, setSelect] = useState<string>('아무거나 선택하세요');
-  const onChange = (target: string) => {
-    setSelect(target);
-  };
-  return (
-    <SmallDiv>
-      <InputDropDown items={dummyOptions} selectValue={[select]} header={select} onChange={onChange} />
-    </SmallDiv>
-  );
-};
-
-export const InputDropDownMultiUsage: React.FC = () => {
-  const [select, setSelect] = useState<Set<string>>(new Set<string>());
-  const header = Array.from(select).join('') === '' ? '아무거나 선택하세요' : Array.from(select).join('');
-  console.log(select);
-  const onChange = (target: string) => {
-    setSelect((state) => {
-      state.add(target);
-      return new Set(Array.from(state));
-    });
-  };
-
-  return (
-    <SmallDiv>
-      <InputDropDown items={dummyOptions} selectValue={[...select]} header={header} multi={true} onChange={onChange} />
+      <SingleInputDropDown placeholder={'Single'} items={dummyOptions} />
     </SmallDiv>
   );
 };
