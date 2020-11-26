8c8c2340386fbeeb05e14338f71da5ea62efc5d3
mu1616 <mu1616@naver.com>
2020-11-26 15:28:44 +0900

    feat: NextButton 컴포넌트 스토리 생성
    - default 일때 스토리 생성
    - wrapper 적용했을 때 스토리 생성

--- client/src/components/next-button/NextButton.stories.tsx
+++ client/src/components/next-button/NextButton.stories.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import NextButton from './NextButton';
+import styled from 'styled-components';
+import ChangeDateContainer from '../change-date-container/ChangeDateContainer';
 
 export default {
   title: 'next-button/NextButton',
   component: NextButton,
 };
 
-export const PreviousButtonDefault: React.FC = () => {
+export const Default: React.FC = () => {
   return <NextButton />;
+};
+
+const Div = styled.div`
+  width: 120px;
+  height: 100px;
+  border: 1px solid black;
+`;
+
+export const SmallNextButton: React.FC = () => {
+  return (
+    <Div>
+      <NextButton />
+    </Div>
+  );
 };
