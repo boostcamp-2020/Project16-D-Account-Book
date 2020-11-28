1f39ce8ebd30f2aa35b5b15170c9a16788bece7b
mu1616 <mu1616@naver.com>
2020-11-28 17:05:22 +0900

    feat: CreateButton 컴포넌트 생성
    - 거래내역 작성버튼 컴포넌트 생성

---
+++ client/src/components/common/menu-navigation/CreateButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import MenuBackground from './MenuBackground';
+
+export const SvgWrapper = styled.svg<{ width: number; height: number }>`
+  position: relative;
+  left: ${({ width }) => `calc(50% - ${width / 2}px)`};
+  top: ${({ height }) => `calc(50% - ${height / 2}px)`};
+`;
+
+interface Props {
+  width: number;
+  height: number;
+}
+
+const FilterButton: React.FC<Props> = ({ width, height }: Props) => {
+  return (
+    <MenuBackground>
+      <SvgWrapper width={width} height={height}>
+        <svg viewBox="0 0 32.24800109863281 32.24800109863281" fill="#000000" data-svg-content="true">
+          <g>
+            <path d="M 21.172,21.172L 19.39,15.792L 9.11,5.512L 5.512,9.11L 15.792,19.39 zM 0.746,0.746c-0.994,0.994-0.994,2.604,0,3.598l 2.648,2.648l 3.598-3.598L 4.344,0.746 C 3.35-0.248, 1.74-0.248, 0.746,0.746zM 30,6L 15.822,6 l 2,2L 30,8 l0,22 L 8,30 L 8,17.822 l-2-2L 6,30 c0,1.104, 0.896,2, 2,2l 22,0 c 1.104,0, 2-0.896, 2-2L 32,8 C 32,6.896, 31.104,6, 30,6z"></path>
+          </g>
+        </svg>
+      </SvgWrapper>
+    </MenuBackground>
+  );
+};
+
+export default FilterButton;
