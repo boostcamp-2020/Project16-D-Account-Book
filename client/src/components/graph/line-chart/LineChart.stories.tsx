cf5fb74d3521d42b52674d2b0bbd6a5f47025114
lacomaco <lacomaco122@gmail.com>
2020-11-28 19:33:01 +0900

    feat : #137 라인그래프 스토리북 추가
    - 라인그래프를 스토리북에서 볼 수 있도록 추가하였습니다.

---
+++ client/src/components/graph/line-chart/LineChart.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import LineChart from './LineChart';
+import { fullTransaction, duplicatedTransaction } from '../../../__dummy-data__/components/graph/dummyTransaction';
+
+export default {
+  component: LineChart,
+  title: 'LineChart',
+};
+
+const Medium = styled.div`
+  width: 700px;
+`;
+
+export const LineChartSmall: React.FC = () => {
+  return (
+    <Medium>
+      <LineChart transactions={fullTransaction} />
+    </Medium>
+  );
+};
+
+export const LineChartManSamll: React.FC = () => {
+  return (
+    <Medium>
+      <LineChart transactions={duplicatedTransaction} />
+    </Medium>
+  );
+};
+
+export const LineChartOne: React.FC = () => {
+  return (
+    <Medium>
+      <LineChart transactions={[{ date: new Date(), value: 10000000000000 }]} />
+    </Medium>
+  );
+};
