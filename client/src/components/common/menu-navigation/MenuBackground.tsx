1158e10a49929c3eb58116b8bc87bcee0c43d174
mu1616 <mu1616@naver.com>
2020-11-28 13:59:10 +0900

    feat: MenuBackground 컴포넌트 생성
    - 메뉴버튼의 회색 배경 컴포넌트를 생성하였습니다.

---
+++ client/src/components/common/menu-navigation/MenuBackground.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import { GRAY } from '../../../constants/color';
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: ${GRAY};
+  cursor: pointer;
+`;
+
+interface Props {
+  children?: JSX.Element;
+}
+
+const MenuBackground: React.FC<Props> = ({ children }: Props) => {
+  return <Background>{children}</Background>;
+};
+
+export default MenuBackground;
