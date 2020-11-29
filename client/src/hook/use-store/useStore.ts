8f019312ad67b60f48a009932129396fc15604f3
mu1616 <mu1616@naver.com>
2020-11-29 19:51:06 +0900

    chore: useStore 주석 추가

--- client/src/hook/use-store/useStore.ts
+++ client/src/hook/use-store/useStore.ts
@@ -4,6 +4,7 @@ interface Store {
   dateStore: DateStore;
 }
 
+// TODO: store 생성 시 이곳에서 return 부분에 추가하면 됩니다! ex) return { dataStore, accountbookStore, xxxStore }
 const useStore = (): Store => {
   return { dateStore };
 };
