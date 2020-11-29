517fc513329cb7bde16f442e14a6dfc14c7bf118
bbbyung2 <bhko0524@naver.com>
2020-11-29 23:30:23 +0900

    feat: kakao oauth 로그인 기능 구현
    - getKakaoToken : kakao 인증 서버에서 발급 받은 인증 코드와 임의의 state를 이용해 access_token 등을 발급 받는 함수 구현
    - getKakaoUserInfo : 앞서 발급된 access_token을 이용하여 kakao에 로그인한 사용자 정보 가져오는 함수 구현
    - findOrCreateKakaoUser : 앞서 가져온 사용자 정보와 일치하는 사용자가 DB의 user 테이블에 존재하면 그대로 반환, 존재하지 않으면 생성 후 반환하는 함수 구현
    - config/oauth.js 파일 리팩토링으로 변경된 변수명을 적용해주는 리팩토링 작업

--- server/src/services/oauth.js
+++ server/src/services/oauth.js
@@ -1,15 +1,16 @@
 const axios = require('axios');
 const jwt = require('jsonwebtoken');
+const qs = require('querystring');
 
-const oauthConfig = require('../config/oauth');
+const { oauthConfig, jwtConfig } = require('../config/oauth');
 const db = require('../models');
 
 const getNaverToken = async (code, state) => {
-  const { data } = await axios.get(oauthConfig.naverUserToken, {
+  const { data } = await axios.get(oauthConfig.naver.userTokenURL, {
     params: {
       grant_type: 'authorization_code',
-      client_id: oauthConfig.naverClientId,
-      client_secret: oauthConfig.naverClientSecret,
+      client_id: oauthConfig.naver.clientId,
+      client_secret: oauthConfig.naver.clientSecret,
       code,
       state,
     },
@@ -19,13 +20,13 @@ const getNaverToken = async (code, state) => {
     accessToken: data.access_token,
     refreshToken: data.refresh_token,
     expiresIn: data.expires_in,
-    tokenType: data.toke_type,
+    tokenType: data.token_type,
   };
 };
 
 const getNaverUserInfo = async (code, state) => {
   const { accessToken } = await getNaverToken(code, state);
-  const { data: userInfo } = await axios.get(oauthConfig.naverUserInfoURL, {
+  const { data: userInfo } = await axios.get(oauthConfig.naver.userInfoURL, {
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
@@ -42,8 +43,8 @@ const generateToken = (user) => {
       nickname: user.nickname,
       profileUrl: user.profileUrl,
     },
-    oauthConfig.jwtSecretKey,
-    { expiresIn: '2h' },
+    jwtConfig.jwtSecretKey,
+    { expiresIn: jwtConfig.jwtExpiresIn },
   );
   return jwtToken;
 };
@@ -61,8 +62,57 @@ const findOrCreateUser = async (data) => {
   return user;
 };
 
+const getKakaoToken = async (code, state) => {
+  const requestParams = {
+    grant_type: 'authorization_code',
+    client_id: oauthConfig.kakao.clientId,
+    client_secret: oauthConfig.kakao.clientSecret,
+    redirect_uri: oauthConfig.kakao.redirectURI,
+    code,
+    state,
+  };
+  const params = qs.stringify(requestParams);
+
+  const { data } = await axios.post(oauthConfig.kakao.userTokenURL, params, {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded;' },
+  });
+
+  return {
+    accessToken: data.access_token,
+    refreshToken: data.refresh_token,
+    expiresIn: data.expires_in,
+    tokenType: data.token_type,
+  };
+};
+
+const getKakaoUserInfo = async (code, state) => {
+  const { accessToken } = await getKakaoToken(code, state);
+  const { data: userInfo } = await axios.get(oauthConfig.kakao.userInfoURL, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return userInfo;
+};
+
+const findOrCreateKakaoUser = async (data) => {
+  const [user] = await db.user.findOrCreate({
+    where: {
+      provider: 'kakao',
+      email: data.kakao_account.email,
+      nickname: data.properties.nickname,
+      profileUrl: data.properties.profile_image,
+    },
+  });
+
+  return user;
+};
+
 module.exports = {
   getNaverUserInfo,
   generateToken,
   findOrCreateUser,
+  getKakaoUserInfo,
+  findOrCreateKakaoUser,
 };
