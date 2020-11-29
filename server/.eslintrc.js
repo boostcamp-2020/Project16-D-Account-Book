52b359e4e8fd90d83208b9bb5e76a5ee037256d4
Mong-Gu <yhw8258@naver.com>
2020-11-29 17:38:03 +0900

    style: lint 적용

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -5,19 +5,19 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: ["airbnb-base", "eslint:recommended", "plugin:prettier/recommended"],
+  extends: ['airbnb-base', 'eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
-    semi: "error",
-    "no-console": "off",
-    "prettier/prettier": [
-      "error",
+    semi: 'error',
+    'no-console': 'off',
+    'prettier/prettier': [
+      'error',
       {
-        endOfLine: "auto",
+        endOfLine: 'auto',
       },
     ],
-    "no-unused-vars": "warn",
+    'no-unused-vars': 'warn',
   },
 };
