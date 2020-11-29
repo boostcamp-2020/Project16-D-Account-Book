f18d58f49b1b4f5191001d70cf1894c97bb55338
lacomaco <lacomaco122@gmail.com>
2020-11-29 23:40:48 +0900

    color : 진한 회색, 진한 빨강색 추가
    - 진한 회색과 진한 빨강색을 color constants에 추가하였습니다.

--- client/src/constants/color.js
+++ client/src/constants/color.js
@@ -2,3 +2,5 @@ export const BLUE = '#1EA7FD';
 export const RED = '#F35454';
 export const GRAY = '#E8E8E8';
 export const DEEP_GRAY = '#7f7f7f';
+export const MODAL_GRAY = '#5E5E5E';
+export const MODAL_RED = '#E74C3C';
