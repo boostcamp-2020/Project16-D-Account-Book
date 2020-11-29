1d5c74819ba9540866d02b8f43ea837a42dd8305
Mong-Gu <yhw8258@naver.com>
2020-11-30 00:45:26 +0900

    feat: 작은 카카오 로그인 버튼 생성 및 스토리북 구현

---
+++ client/src/components/login-button/kakao-login-button/small-button/SmallKakaoLoginButton.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SmallKakaoLoginButton from './SmallKakaoLoginButton';
+
+export default {
+  title: 'Login-Page/KakaoLoginButton',
+  component: SmallKakaoLoginButton,
+};
+
+export const SmallButton = (): JSX.Element => {
+  return <SmallKakaoLoginButton />;
+};
