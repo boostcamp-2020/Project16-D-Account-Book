268b5387dd75eda82dd7f32f6456417aff0d8fda
mu1616 <mu1616@naver.com>
2020-11-30 00:59:53 +0900

    fix: dateStore 오류 수정 및 리팩토링
    - endDate가 변하지 않는 오류 수정
    - 반복되는 로직은 util 함수로 분리

--- client/src/store/dateStore.tsx
+++ client/src/store/dateStore.tsx
@@ -1,5 +1,5 @@
 import { observable, action, makeObservable } from 'mobx';
-import { isEndOfYear, isStartOfYear } from '../utils/date';
+import { getFirstDateOfPreviousMonth, getFirstDateOfNextMonth } from '../utils/date';
 import React, { createContext } from 'react';
 
 export default class DateStore {
@@ -11,40 +11,19 @@ export default class DateStore {
     const currentDate = new Date();
     this.startDate = new Date(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-1`);
     this.startDate.setHours(0, 0, 0, 0);
-    this.endDate = new Date(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-1`);
-    this.endDate.setHours(0, 0, 0, 0);
+    this.endDate = getFirstDateOfNextMonth(this.startDate);
   }
 
   @action
   moveToNextMonth(): void {
-    let year = this.startDate.getFullYear();
-    let month = this.startDate.getMonth() + 1;
-
-    if (isEndOfYear(month)) {
-      year = year + 1;
-      month = 1;
-    } else {
-      month = month + 1;
-    }
-
-    this.startDate = new Date(`${year}-${month}-1`);
-    this.startDate.setHours(0, 0, 0, 0);
+    this.startDate = getFirstDateOfNextMonth(this.startDate);
+    this.endDate = getFirstDateOfNextMonth(this.startDate);
   }
 
   @action
   moveToPreviousMonth(): void {
-    let year = this.startDate.getFullYear();
-    let month = this.startDate.getMonth() + 1;
-
-    if (isStartOfYear(month)) {
-      year = year - 1;
-      month = 12;
-    } else {
-      month = month - 1;
-    }
-
-    this.startDate = new Date(`${year}-${month}-1`);
-    this.startDate.setHours(0, 0, 0, 0);
+    this.startDate = getFirstDateOfPreviousMonth(this.startDate);
+    this.endDate = getFirstDateOfNextMonth(this.startDate);
   }
 }
 
