afcbc1721a5f52d3a8f04faf1e935e5f9824b053
lacomaco <lacomaco122@gmail.com>
2020-11-29 23:41:36 +0900

    feat : 기획서 4페이지. 모달 창 생성
    - 이슈에는 아직 없는 기획서의 4페이지 모달창을 생성했습니다.
    
    - 우선은 상태값이 없기 때문에 글자와 텍스트만 띄우는 정도로
      생성했습니다.

---
+++ client/src/components/common/modals/modals.stories.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import AcoountbookDeleteByAdminModal from './AcoountbookDeleteByAdminModal';
+
+export default {
+  title: 'Modal Example',
+};
+
+export const AccountbookDeleteByAdminDefault: React.FC = () => {
+  const [state, setState] = useState<boolean>(true);
+
+  const closeModal = () => {
+    console.log('outside click');
+  };
+  const cancelClick = () => {
+    console.log('click cancel');
+  };
+
+  const deleteClick = () => {
+    console.log('delete Click');
+  };
+  return (
+    <AcoountbookDeleteByAdminModal
+      cancelClick={cancelClick}
+      deleteClick={deleteClick}
+      show={state}
+      closeModal={closeModal}
+    />
+  );
+};
