059162148a99974b9ab1a67b649f24cfdfaaf994
lacomaco <lacomaco122@gmail.com>
2020-11-30 10:35:18 +0900

    fix : 파일명 오타 변경
    - Acoountbook -> Accountbook으로 변경하였습니다.

--- client/src/components/common/modals/modals.stories.tsx
+++ client/src/components/common/modals/modals.stories.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import AcoountbookDeleteByAdminModal from './AcoountbookDeleteByAdminModal';
+import AcoountbookDeleteByAdminModal from './AccountbookDeleteByAdminModal';
 
 export default {
   title: 'Modal Example',
