696f722690a52fa532df6dc6327ea3fcd1e8b75a
Mong-Gu <yhw8258@naver.com>
2020-12-01 01:23:02 +0900

    feat: 큰 모양의 카카오로그인 버튼 컴포넌트 생성

--- client/src/components/login-button/kakao-login-button/large-button/LargeKakaoLoginButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import largeKakaoLoginButtonImage from '../../../../assets/image/kakao-login-large.png';
-import LoginButton from '../../../../types/loginButton';
-
-const LargeKakaoLoginButton: React.FC<LoginButton> = ({ onClick }: LoginButton) => {
-  return <img src={String(largeKakaoLoginButtonImage)} alt="kakao_login_button" onClick={onClick} />;
-};
-
-export default LargeKakaoLoginButton;
