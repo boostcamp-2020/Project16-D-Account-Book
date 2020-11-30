055bd393d9ea0fad3415ff3243c179b93b7e41d7
lacomaco <lacomaco122@gmail.com>
2020-11-30 19:55:00 +0900

    feat : 내역작성 모달창의 인풋들이 모여있는 content 뼈대 작성

---
+++ client/src/components/common/modals/form-modal-transaction/TransactionInputList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const InputListWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const TransactionInputList: React.FC = () => {
+  return <InputListWrapper>zz</InputListWrapper>;
+};
+
+export default TransactionInputList;
