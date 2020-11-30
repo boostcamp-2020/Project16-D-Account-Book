cf5c6b991b7091967ec4ba2ee370fcf8b71c66bf
lacomaco <lacomaco122@gmail.com>
2020-11-30 19:50:07 +0900

    feat : 모달의 헤더 ( 모달 닫기 버튼,완료버튼 mms 버튼 ) 구현
    - MMS 버튼은 아직 구현되있지 않아 우선 비워두었습니다.
    - 모든 상태를 props로 받는 View 만 구현해두었습니다.

---
+++ client/src/components/common/modals/form-modal-header/FormModalHeader.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import styled from 'styled-components';
+import ModalBackButton from '../../back-button/ModalBackButton';
+
+interface IFormModalHeaderProps {
+  closeModal?: () => void;
+  clickMMS?: () => void;
+  clickRed?: () => void;
+  clickBlue?: () => void;
+  modalName?: string;
+  blueName?: string;
+  redName?: string;
+  mms?: boolean;
+}
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const CloseModalButtonWrapper = styled.div`
+  cursor: pointer;
+  width: 1.3em;
+  margin-right: 1em;
+`;
+
+const Right = styled.p`
+  margin-left: auto;
+`;
+
+const ModalName = styled.p`
+  cursor: pointer;
+  font-size: 1.3em;
+`;
+
+const RedButton = styled.p`
+  cursor: pointer;
+  color:tomato;
+  margin-right: 1em;
+`;
+
+const BlueButton = styled.p`
+  cursor: pointer;
+  color:dodgerblue;
+`;
+
+const FormModalHeader: React.FC<IFormModalHeaderProps> = ({
+  closeModal,
+  clickMMS,
+  clickRed,
+  clickBlue,
+  modalName,
+  blueName,
+  redName,
+  mms,
+}: IFormModalHeaderProps) => {
+  return (
+    <Header>
+      <CloseModalButtonWrapper>
+        <ModalBackButton onClick={closeModal} />
+      </CloseModalButtonWrapper>
+      <ModalName>{modalName}</ModalName>
+      <Right />
+      {mms}
+      {redName && <RedButton onClick={clickRed}>{redName}</RedButton>}
+      {blueName && <BlueButton onClick={clickBlue}>{blueName}</BlueButton>}
+    </Header>
+  );
+};
+
+export default FormModalHeader;
