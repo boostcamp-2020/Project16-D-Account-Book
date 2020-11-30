d7af2212fe13e6ac01b4c7bb61de8cd0c1755603
Mong-Gu <yhw8258@naver.com>
2020-12-01 01:23:11 +0900

    feat: 작은 모양의 카카오로그인 버튼 컴포넌트 생성

--- client/src/components/login-button/kakao-login-button/small-button/SmallKakaoLoginButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import smallKakaoLoginButtonImage from '../../../../assets/image/kakao-login-small.png';
-import LoginButton from '../../../../types/loginButton';
-
-const SmallKakaoLoginButton: React.FC<LoginButton> = ({ onClick }: LoginButton) => {
-  return <img src={String(smallKakaoLoginButtonImage)} alt="kakao_login_button" onClick={onClick} />;
-};
-
-export default SmallKakaoLoginButton;
