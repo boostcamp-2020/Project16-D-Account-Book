6ec56832e84cd5c42861faadc64f583d3fab04a7
mu1616 <mu1616@naver.com>
2020-11-30 19:47:23 +0900

    feat: expenditure 조회 서비스 함수 생성

--- server/src/services/transaction.js
+++ server/src/services/transaction.js
@@ -2,7 +2,7 @@ const db = require('@models');
 const { Op } = require('sequelize');
 
 const findIncomes = async (accountbookId, startDate, endDate) => {
-  const transactions = await db.income.findAll({
+  const incomes = await db.income.findAll({
     attributes: ['id', 'amount', 'content', 'date', 'memo'],
     where: {
       accountbookId,
@@ -24,9 +24,36 @@ const findIncomes = async (accountbookId, startDate, endDate) => {
     ],
   });
 
-  return transactions;
+  return incomes;
+};
+
+const findExpenditures = async (accountbookId, startDate, endDate) => {
+  const expenditures = await db.expenditure.findAll({
+    attributes: ['id', 'amount', 'place', 'date', 'memo'],
+    where: {
+      accountbookId,
+      date: {
+        [Op.gte]: new Date(startDate),
+        [Op.lt]: new Date(endDate),
+      },
+    },
+    include: [
+      {
+        model: db.expenditureCategory,
+        as: 'category',
+        attributes: ['id', 'name', 'color'],
+      },
+      {
+        model: db.account,
+        attributes: ['id', 'name', 'color'],
+      },
+    ],
+  });
+
+  return expenditures;
 };
 
 module.exports = {
   findIncomes,
+  findExpenditures,
 };
