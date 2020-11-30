ad4dff9271872e73a964fc0d3a02f73b05b689f4
mu1616 <mu1616@naver.com>
2020-11-30 19:47:43 +0900

    feat: transactions 조회 컨트롤러 함수 생성

---
+++ server/src/api/controllers/transaction.js
@@ -0,0 +1,13 @@
+const transactionService = require('@services/transaction');
+
+const getTransactions = async (ctx) => {
+  const { start_date, end_date, accountbook_id } = ctx.request.query;
+  const incomes = await transactionService.findIncomes(accountbook_id, start_date, end_date);
+  const expenditures = await transactionService.findExpenditures(accountbook_id, start_date, end_date);
+  const transactions = [...incomes, ...expenditures];
+  ctx.body = transactions;
+};
+
+module.exports = {
+  getTransactions,
+};
