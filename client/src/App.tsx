095e510696abc1a294d2bf669a7fbc0247924b6a
mu1616 <mu1616@naver.com>
2020-12-01 00:45:49 +0900

    fix: 라우팅 수정
    - 기존에 테스트를 위해 사용했던 코드들을 지웠습니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import TransactionPage from './pages/transaction-page/TransactionPage';
-import { TransactionProvider } from './store/TransactionStore';
-import { DateProvider } from './store/DateStore';
 
 function App(): JSX.Element {
   return (
     <Router>
       <Switch>
-        <DateProvider>
-          <Route exact path="/accountbooks/:id/transactions" component={TransactionPage} />
-        </DateProvider>
-      </Switch>
-      <Switch>
-        <Route exact path="/accountbook/:id/transactions" component={TransactionPage} />
+        <Route exact path="/accountbooks/:id/transactions" component={TransactionPage} />
       </Switch>
     </Router>
   );
