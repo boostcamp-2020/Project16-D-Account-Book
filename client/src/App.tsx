a088ea6b31e4f5378d93c223279d57bb8ad1d710
Mong-Gu <yhw8258@naver.com>
2020-12-01 01:19:40 +0900

    feat: Global fonts 적용하여 App 초기화면 구축

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import LoginPage from './pages/login-page/LoginPage';
+import GlobalFonts from './assets/fonts/GlobalFonts';
 
 function App(): JSX.Element {
-  return <div>하이</div>;
+  return (
+    <>
+      <GlobalFonts />
+      <LoginPage />
+    </>
+  );
 }
 
 export default App;
