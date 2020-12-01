b06f13944eae9e1debe749c9fab42fdb5c1144ae
bbbyung2 <bhko0524@naver.com>
2020-12-02 01:58:55 +0900

    feat: InputRadio 스토리 파일 생성

---
+++ client/src/components/common/inputs/input-radio/InputRadio.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import InputRadio from './InputRadio';
+
+export default {
+  title: 'inputs',
+  component: InputRadio,
+};
+
+export const InputRadioDefault: React.FC = () => {
+  return <InputRadio />;
+};
