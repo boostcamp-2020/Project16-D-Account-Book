fb210239c9221af1b8c713dd1f98489f7b6c1fc4
Mong-Gu <yhw8258@naver.com>
2020-12-01 23:42:24 +0900

    refactor: jwt token 발급할 때 payload의 claim 컨벤션 준수하기
    claim에 다음과 같은 클레임을 담았습니다.
    - 등록된 클레임: iss
    - 비공개 클레임: userId, provider, nickname

--- server/src/utils/jwt-utils.js
+++ server/src/utils/jwt-utils.js
@@ -4,10 +4,10 @@ const jwtConfig = require('@config/jwt');
 const generateToken = (user) => {
   const jwtToken = jwt.sign(
     {
+      iss: 'moa',
+      userId: user.id,
       provider: user.provider,
-      email: user.email,
-      nickname: user.nickname,
-      profileUrl: user.profileUrl,
+      nickname: user.email.match(/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@/g)[0].replace('@', ''),
     },
     jwtConfig.jwtSecretKey,
     { expiresIn: jwtConfig.jwtExpiresIn },
