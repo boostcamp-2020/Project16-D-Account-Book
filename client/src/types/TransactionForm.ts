1f54f060646c62d538e3d4615a3e5eb74d36b7cf
lacomaco <lacomaco122@gmail.com>
2020-12-01 15:34:29 +0900

    feat : useForm Reducer 타입 추가
    - types 폴더에 useForm Reducer에 쓰이는 액션타입과 state 타입을 선언하고
      정리해놓았습니다.

---
+++ client/src/types/TransactionForm.ts
@@ -0,0 +1,78 @@
+import { Options } from './options';
+
+export interface ITransactionForm {
+  classify: boolean | undefined;
+  price: number | undefined;
+  categories: string | undefined;
+  accounts: string | undefined;
+  content: string | undefined;
+  date: string | undefined;
+  memo: string | undefined;
+}
+
+export interface ITransactionFormChange {
+  classify?: {
+    income: () => void;
+    expenditure: () => void;
+  };
+  price?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  categories: (change: string) => void;
+  accounts: (change: string) => void;
+  content?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  date?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  memo?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export enum FormActionType {
+  CLASSIFY_CHANGE,
+  PRICE_CHANGE,
+  CATEGORIES_CHANGE,
+  ACCOUNTS_CHANGE,
+  CONTENT_CHANGE,
+  DATE_CHANGE,
+  MEMO_CHANGE,
+}
+
+interface CLASSIFY_CHANGE_ACTION {
+  data: boolean;
+  type: FormActionType.CLASSIFY_CHANGE;
+}
+
+interface PRICE_CHANGE_ACTION {
+  data: number;
+  type: FormActionType.PRICE_CHANGE;
+}
+
+interface CATEGORIES_CHANGE {
+  data: string;
+  type: FormActionType.CATEGORIES_CHANGE;
+}
+
+interface ACCOUNTS_CHANGE_ACTION {
+  data: string;
+  type: FormActionType.ACCOUNTS_CHANGE;
+}
+
+interface CONTENT_CHANGE_ACTION {
+  data: string;
+  type: FormActionType.CONTENT_CHANGE;
+}
+
+interface DATE_CHANGE_ACTION {
+  data: string;
+  type: FormActionType.DATE_CHANGE;
+}
+
+interface MEMO_CHANGE_ACTION {
+  data: string;
+  type: FormActionType.MEMO_CHANGE;
+}
+
+export type FormChangeAction =
+  | CLASSIFY_CHANGE_ACTION
+  | PRICE_CHANGE_ACTION
+  | CATEGORIES_CHANGE
+  | ACCOUNTS_CHANGE_ACTION
+  | CONTENT_CHANGE_ACTION
+  | DATE_CHANGE_ACTION
+  | MEMO_CHANGE_ACTION;
