166c38d832a73a8dcd70b7a0a4dcaa7953b08f9f
Mong-Gu <yhw8258@naver.com>
2020-12-01 16:32:05 +0900

    refactor: wdith props 제거 및 고정된 버튼 크기로 지정

--- client/src/components/login-button/kakao-login-button/LargeKakaoLoginButton.tsx
+++ client/src/components/login-button/kakao-login-button/LargeKakaoLoginButton.tsx
@@ -3,18 +3,18 @@ import styled from 'styled-components';
 import largeKakaoLoginButtonImage from '../../../assets/images/kakao-login-large.png';
 import LoginButton from '../../../types/loginButton';
 
-const StyledDiv = styled.div<{ width: number }>`
+const StyledDiv = styled.div`
   cursor: pointer;
   margin-right: 10px;
   img {
-    width: ${(props) => props.width};
+    width: 200px;
   }
 `;
 
-const LargeKakaoLoginButton: React.FC<LoginButton> = ({ onClick, width }: LoginButton) => {
+const LargeKakaoLoginButton: React.FC<LoginButton> = ({ onClick }: LoginButton) => {
   return (
-    <StyledDiv onClick={onClick} width={width}>
-      <img src={String(largeKakaoLoginButtonImage)} alt="kakao_login_button" width="185" />
+    <StyledDiv onClick={onClick}>
+      <img src={String(largeKakaoLoginButtonImage)} alt="kakao_login_button" />
     </StyledDiv>
   );
 };
