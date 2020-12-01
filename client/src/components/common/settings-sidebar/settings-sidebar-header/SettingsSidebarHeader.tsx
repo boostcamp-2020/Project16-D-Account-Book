cd8bad7f348d407f4df68fdefba3ca0151872937
bbbyung2 <bhko0524@naver.com>
2020-12-01 14:22:03 +0900

    feat: 가계부 관리 페이지 좌측 Sidebar의 Header 컴포넌트 구현
    - SettingsSidebarHeader 컴포넌트는 클릭 시 이전 페이지로 이동하는 ModalBackButton과 페이지 이름인 '가계부 관리'가 포함
    - SettingsSidebarHeader 컴포넌트 스토리 파일 생성

---
+++ client/src/components/common/settings-sidebar/settings-sidebar-header/SettingsSidebarHeader.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import ModalBackButton from '../../back-button/ModalBackButton';
+
+interface SettingsSidebarHeaderProps {
+  goToPreviousPage?: () => void;
+}
+
+const SidebarHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const BackButtonWrapper = styled.div`
+  cursor: pointer;
+  width: 1.3em;
+  margin-right: 2.5em;
+  margin-left: 0.3em;
+`;
+
+const SettingsTitle = styled.div`
+  padding-top: 0.3em;
+  font-size: 1.4rem;
+`;
+
+const SettingsSidebarHeader = ({ goToPreviousPage }: SettingsSidebarHeaderProps): JSX.Element => {
+  return (
+    <SidebarHeader>
+      <BackButtonWrapper>
+        <ModalBackButton onClick={goToPreviousPage} />
+      </BackButtonWrapper>
+      <SettingsTitle>가계부 관리</SettingsTitle>
+    </SidebarHeader>
+  );
+};
+
+export default SettingsSidebarHeader;
