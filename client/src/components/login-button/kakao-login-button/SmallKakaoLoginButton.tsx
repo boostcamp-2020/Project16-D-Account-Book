199a8722123ffd581c0f5fc89f8200e1c82ebdef
Mong-Gu <yhw8258@naver.com>
2020-12-01 16:33:06 +0900

    refactor: wdith props 제거 및 고정된 버튼 크기로 지정, 반응형 웹 고려
    - 인터페이스에서 width를 제거하여 props에서도 width 제거
    - media를 고려하여 고정된 크기의 버튼으로 변경

--- client/src/components/login-button/kakao-login-button/SmallKakaoLoginButton.tsx
+++ client/src/components/login-button/kakao-login-button/SmallKakaoLoginButton.tsx
@@ -3,17 +3,25 @@ import styled from 'styled-components';
 import smallKakaoLoginButtonImage from '../../../assets/images/kakao-login-small.png';
 import LoginButton from '../../../types/loginButton';
 
-const StyledDiv = styled.div<{ width: number }>`
+const StyledDiv = styled.div`
   cursor: pointer;
-  margin-right: 10px;
+  margin-right: 50px;
   img {
-    width: ${(props) => props.width};
+    width: 120px;
+  }
+
+  @media screen and (max-width: 600px) {
+    margin-right: 0px;
+    margin-bottom: 20px;
+    img {
+      width: 80px;
+    }
   }
 `;
-const SmallKakaoLoginButton: React.FC<LoginButton> = ({ onClick, width }: LoginButton) => {
+const SmallKakaoLoginButton: React.FC<LoginButton> = ({ onClick }: LoginButton) => {
   return (
-    <StyledDiv onClick={onClick} width={width}>
-      <img src={String(smallKakaoLoginButtonImage)} alt="kakao_login_button" width="120" />
+    <StyledDiv onClick={onClick}>
+      <img src={String(smallKakaoLoginButtonImage)} alt="kakao_login_button" />
     </StyledDiv>
   );
 };
