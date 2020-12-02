08e3d60bf281054457ee6935a9e9fafc89809845
mu1616 <mu1616@naver.com>
2020-12-02 15:44:14 +0900

    feat: FilterOption 스토리 생성

---
+++ client/src/components/transaction-page/filter-option/FilterOption.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import FilterOption from './FilterOption';
+import styled from 'styled-components';
+
+export default {
+  title: 'transaction-page/FilterOption',
+};
+
+const Wrapper = styled.div`
+  width: 220px;
+`;
+
+const query = {
+  startDate: '2020.10.01',
+  endDate: '2020.12.01',
+  account: '현금+농협',
+  incomeCategory: '급여',
+  expenditureCategory: '쇼핑',
+};
+
+export const Default = (): JSX.Element => {
+  return <FilterOption query={query} />;
+};
+
+export const ApplyWrapper = (): JSX.Element => {
+  return (
+    <Wrapper>
+      <FilterOption query={query} />
+    </Wrapper>
+  );
+};
