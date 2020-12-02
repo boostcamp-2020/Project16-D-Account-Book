e50930453cda60ba8a7c294402d477c1d85e4eb0
bbbyung2 <bhko0524@naver.com>
2020-12-02 18:02:00 +0900

    test: 카테고리 관리 페이지에서 사용할 categoryData 추가
    - expenditureCategories와 incomeCategories로 나눠서 생성

---
+++ client/src/__dummy-data__/components/settings/categoryData.ts
@@ -0,0 +1,25 @@
+export const expenditureCategories = [
+  { id: 1, name: '식사', color: '#ed8484' },
+  { id: 2, name: '카페/간식', color: '#9B59B6' },
+  { id: 3, name: '의료/건강', color: '#F1C40F' },
+  { id: 4, name: '술/유흥', color: '#BF857F' },
+  { id: 5, name: '의류', color: '#3498DB' },
+  { id: 6, name: '육아', color: '#F39C12' },
+  { id: 7, name: '교통', color: '#569772' },
+  { id: 8, name: '주거', color: '#7A88F6' },
+  { id: 9, name: '통신', color: '#1ABC9C' },
+  { id: 10, name: '생활', color: '#EC60C9' },
+  { id: 11, name: '문화/여가', color: '#958A5F' },
+  { id: 12, name: '여행/숙박', color: '#CEA0FF' },
+  { id: 13, name: '교육', color: '#27AE60' },
+  { id: 14, name: '경조사', color: '#97AAAC' },
+];
+
+export const incomeCategories = [
+  { id: 1, name: '타행이체', color: '#9FEADB' },
+  { id: 2, name: '더치페이', color: '#7ABC1A' },
+  { id: 3, name: '급여', color: '#E19CCD' },
+  { id: 4, name: '용돈', color: '#1ABC9C' },
+  { id: 5, name: '사업수입', color: '#75A3C1' },
+  { id: 6, name: '금융수입', color: '#EDCE9E' },
+];
