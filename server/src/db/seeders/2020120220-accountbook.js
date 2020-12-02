12d0003b6f52ff80807e2e4a657e4c18e79c5f00
Mong-Gu <yhw8258@naver.com>
2020-12-02 18:14:04 +0900

    feat: accountbook 더미데이터 seed 추가

---
+++ server/src/db/seeders/2020120220-accountbook.js
@@ -0,0 +1,19 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('accountbook', [
+      {
+        id: 1,
+        title: 'accountbook1',
+        gmt: 9,
+        start_day: true,
+        created_at: '2020-11-23',
+        updated_at: '2020-11-23',
+        deleted_at: null,
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('accountbook', null, {});
+  },
+};
