c29ff84e5456df5237ff13535d09b0c8264e49aa
bbbyung2 <bhko0524@naver.com>
2020-12-03 02:01:04 +0900

    feat: 카테고리 관리 페이지에서 새로운 카테고리 생성하는 컴포넌트 추가
    - Category 컴포넌트들 마지막에 AddCategoryButton 컴포넌트 추가

--- client/src/pages/settings-categories-page/SettingsCategoriesPage.tsx
+++ client/src/pages/settings-categories-page/SettingsCategoriesPage.tsx
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import SettingsSidebar from '../../components/common/settings-sidebar/SettingsSidebar';
 import Category from '../../components/common/category/Category';
-import { expenditureCategories, incomeCategories } from '../../__dummy-data__/components/settings/categoryData';
+import AddCategoryButton from '../../components/common/add-category-button/AddCategoryButton';
+import {
+  expenditureCategoriesDummy,
+  incomeCategoriesDummy,
+} from '../../__dummy-data__/components/settings/categoryData';
 
 const SettingsCategoriesPageWrapper = styled.div`
   display: flex;
@@ -36,9 +40,14 @@ const CategoryItemWrapper = styled.div`
   width: 20%;
   margin-bottom: 3vh;
   margin-right: 2vw;
+  cursor: pointer;
 `;
 
 const SettingsCategoriesPage = (): JSX.Element => {
+  // TODO: setter의 경우 카테고리 생성, 변경 모달창 구현 시 작업 예정
+  const [expenditureCategories, setExpenditureCategories] = useState(expenditureCategoriesDummy);
+  const [incomeCategories, setIncomeCategories] = useState(incomeCategoriesDummy);
+
   const ExpenditureCategoryItems = expenditureCategories.map((item) => (
     <CategoryItemWrapper key={item.id}>
       <Category key={item.id} text={item.name} bgColor={item.color} />
@@ -57,11 +66,21 @@ const SettingsCategoriesPage = (): JSX.Element => {
       <SettingsBody>
         <SettingsItemWrapper>
           <Label>지출</Label>
-          <CategoryWrapper>{ExpenditureCategoryItems}</CategoryWrapper>
+          <CategoryWrapper>
+            {ExpenditureCategoryItems}
+            <CategoryItemWrapper>
+              <AddCategoryButton />
+            </CategoryItemWrapper>
+          </CategoryWrapper>
         </SettingsItemWrapper>
         <SettingsItemWrapper>
           <Label>수입</Label>
-          <CategoryWrapper>{IncomeCategoryItems}</CategoryWrapper>
+          <CategoryWrapper>
+            {IncomeCategoryItems}
+            <CategoryItemWrapper>
+              <AddCategoryButton />
+            </CategoryItemWrapper>
+          </CategoryWrapper>
         </SettingsItemWrapper>
       </SettingsBody>
     </SettingsCategoriesPageWrapper>
