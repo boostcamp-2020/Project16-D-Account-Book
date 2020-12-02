e91036e8e1d90f50d5fb2db6d2cdbb83fc32fd93
Mong-Gu <yhw8258@naver.com>
2020-12-02 12:22:15 +0900

    refactor: 코드 리뷰 때 참고용으로 작성했던 코드 제거

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -1,17 +1,10 @@
 const Router = require('koa-router');
 const oauthRouter = require('@routes/oauth');
 const transactionsRouter = require('@routes/transactions');
-const jwtMiddleware = require('@middlewares/jwt-authentication');
 
 const router = new Router();
 
 router.use('/oauth', oauthRouter.routes());
 router.use('/transactions', transactionsRouter.routes());
-
-// test를 위한 jwt middleware
-// 2020.12.02 오전 스크럼 이후 삭제 예정
-router.get('/', jwtMiddleware, (ctx) => {
-  ctx.body = 'Hello World';
-});
 
 module.exports = router;
