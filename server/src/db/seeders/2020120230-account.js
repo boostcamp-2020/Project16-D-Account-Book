5f217150c355e511197c40e739b6a9d4c58c1b3c
Mong-Gu <yhw8258@naver.com>
2020-12-02 18:14:23 +0900

    feat: account 더미데이터 seed 추가

---
+++ server/src/db/seeders/2020120230-account.js
@@ -0,0 +1,55 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('account', [
+      {
+        id: 1,
+        name: '삼성카드',
+        color: 'tomato',
+        accountbook_id: '1',
+        deleted_at: null,
+        created_at: '2020-11-23',
+        updated_at: '2020-11-23',
+      },
+      {
+        id: 2,
+        name: '국민카드',
+        color: 'dodgerblue',
+        accountbook_id: '1',
+        deleted_at: null,
+        created_at: '2020-11-24',
+        updated_at: '2020-11-24',
+      },
+      {
+        id: 3,
+        name: '농협카드',
+        color: 'red',
+        accountbook_id: '1',
+        deleted_at: null,
+        created_at: '2020-11-25',
+        updated_at: '2020-11-25',
+      },
+      {
+        id: 4,
+        name: '우리카드',
+        color: 'blue',
+        accountbook_id: '1',
+        deleted_at: null,
+        created_at: '2020-11-26',
+        updated_at: '2020-11-26',
+      },
+      {
+        id: 5,
+        name: '신한카드',
+        color: 'green',
+        accountbook_id: '1',
+        deleted_at: null,
+        created_at: '2020-11-27',
+        updated_at: '2020-11-27',
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('account', null, {});
+  },
+};
