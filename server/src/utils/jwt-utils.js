177ed4720e7186279d464fe5d31f4fe7b7e5f372
Mong-Gu <yhw8258@naver.com>
2020-12-03 17:18:52 +0900

    refactor: jwt 해독 함수 수정
    - 에러가 날 수 있는 구간마다 에러 핸들링 추가
    - 유저 db를 조회하여 token값 대조하는 부분 추가

--- server/src/utils/jwt-utils.js
+++ server/src/utils/jwt-utils.js
@@ -19,8 +19,22 @@ const generateToken = async (user) => {
 };
 
 const decodeToken = (token) => {
-  const decodedToken = jwt.verify(token, jwtConfig.jwtSecretKey);
-  return decodedToken;
+  jwt.verify(token, jwtConfig.jwtSecretKey, async (err, decoded) => {
+    // jwt secret이 잘못되었다면
+    if (err) {
+      throw new Error('jwt secret이 잘못되었음');
+    }
+    const user = await db.user.findByPk(decoded.id);
+    // 유저가 존재하지 않는다면
+    if (!user) {
+      throw new Error('decoded payload에 기재된 유저가 없음');
+    }
+    // 유저가 존재하지만 토큰값이 일치하지 않는다면
+    if (user.token !== token) {
+      throw new Error('decoded payload에 기재된 유저는 있지만, 서버에서 발행해준 jwt값과 일치하지 않음');
+    }
+    return [user, decoded];
+  });
 };
 
 module.exports = {
