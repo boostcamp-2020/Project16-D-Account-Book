0a3fa4dbb17fd1342fe4fdcf4dfe382ea95c738d
Mong-Gu <yhw8258@naver.com>
2020-12-03 17:17:34 +0900

    refactor: jwt 발급하는 함수 수정
    jwt 발급하면서 user테이블의 token 속성을 업데이트

--- server/src/utils/jwt-utils.js
+++ server/src/utils/jwt-utils.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
-const jwtConfig = require('@config/jwt');
 
-const generateToken = (user) => {
-  const jwtToken = jwt.sign(
+const jwtConfig = require('@config/jwt');
+const db = require('@models');
+
+const generateToken = async (user) => {
+  const jwtToken = jwt.sign({ iss: 'moa', userId: user.id }, jwtConfig.jwtSecretKey, {
+    expiresIn: jwtConfig.jwtExpiresIn,
+  });
+  await db.user.update(
+    { token: jwtToken },
     {
-      iss: 'moa',
-      userId: user.id,
-      provider: user.provider,
-      email: user.email,
-      nickname: user.email.match(/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@/g)[0].replace('@', ''),
-      profileUrl: user.profileUrl,
+      where: {
+        id: user.id,
+      },
     },
-    jwtConfig.jwtSecretKey,
-    { expiresIn: jwtConfig.jwtExpiresIn },
   );
   return jwtToken;
 };
