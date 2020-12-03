9e7525bda58accba9500878a1e0420123c5bce1e
mu1616 <mu1616@naver.com>
2020-12-03 11:45:15 +0900

    feat: 필터모달에서 사용할 dateOption 데이터 추가

---
+++ client/src/__dummy-data__/store/formFilterStore.ts
@@ -0,0 +1,39 @@
+import datePeriod from '../../constants/datePeriod';
+
+export const dateOptions = [
+  {
+    label: datePeriod.ALL,
+    value: datePeriod.ALL,
+    checked: false,
+  },
+  {
+    label: datePeriod.LAST_ONE_WEEK,
+    value: datePeriod.LAST_ONE_WEEK,
+    checked: false,
+  },
+  {
+    label: datePeriod.LAST_ONE_MONTH,
+    value: datePeriod.LAST_ONE_MONTH,
+    checked: false,
+  },
+  {
+    label: datePeriod.LAST_THREE_MONTH,
+    value: datePeriod.LAST_THREE_MONTH,
+    checked: false,
+  },
+  {
+    label: datePeriod.LAST_SIX_MONTH,
+    value: datePeriod.LAST_SIX_MONTH,
+    checked: false,
+  },
+  {
+    label: datePeriod.LAST_ONE_YEAR,
+    value: datePeriod.LAST_ONE_YEAR,
+    checked: false,
+  },
+  {
+    label: datePeriod.USER_SETTING,
+    value: datePeriod.USER_SETTING,
+    checked: false,
+  },
+];
