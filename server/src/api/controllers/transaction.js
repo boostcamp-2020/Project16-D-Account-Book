8343d92f01e79e499c9acf87562a3ffcea396af4
Mong-Gu <yhw8258@naver.com>
2020-12-04 01:16:00 +0900

    style: lint warning 무시해주는 코드 삽입

--- server/src/api/controllers/transaction.js
+++ server/src/api/controllers/transaction.js
@@ -1,6 +1,7 @@
 const transactionService = require('@services/transaction');
 
 const findTransactions = async (ctx) => {
+  // eslint-disable-next-line camelcase
   const { start_date, end_date, accountbook_id } = ctx.request.query;
   const incomes = await transactionService.findIncomes(accountbook_id, start_date, end_date);
   const expenditures = await transactionService.findExpenditures(accountbook_id, start_date, end_date);
