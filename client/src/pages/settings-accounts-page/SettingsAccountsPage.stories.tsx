773dc329dc3362b45d2352735755c306573025fd
bbbyung2 <bhko0524@naver.com>
2020-12-03 14:20:38 +0900

    feat: 결제수단 관리 페이지 구현 및 스토리 파일 생성

---
+++ client/src/pages/settings-accounts-page/SettingsAccountsPage.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SettingsAccountsPage from './SettingsAccountsPage';
+
+export default {
+  title: 'settings-accounts-page/settings-accounts-page',
+  component: SettingsAccountsPage,
+};
+
+export const Default = (): JSX.Element => {
+  return <SettingsAccountsPage />;
+};
