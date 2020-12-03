f1b8520bb3cc7ec5ed33863cf6a865b043b5552a
bbbyung2 <bhko0524@naver.com>
2020-12-03 14:14:26 +0900

    refactor: Account 컴포넌트 width 및 height 조절

--- client/src/components/common/account/Account.tsx
+++ client/src/components/common/account/Account.tsx
@@ -3,10 +3,8 @@ import styled from 'styled-components';
 import { getTextColor } from '../../../utils/color';
 
 const AccountWrapper = styled.div<{ bgColor: string; textColor: string; shadow?: boolean }>`
-  width: 100%;
-  max-width: 300px;
-  height: 100%;
-  max-height: 190px;
+  width: 17vw;
+  height: 10vw;
   background-color: ${({ bgColor }) => bgColor};
   color: ${({ textColor }) => textColor};
   box-sizing: border-box;
