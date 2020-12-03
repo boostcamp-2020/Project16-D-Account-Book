30d5d7d9c94a717bc679118d7f3afce0296da96a
Mong-Gu <yhw8258@naver.com>
2020-12-03 17:45:11 +0900

    fix: async/await가 정상적으로 동작하지 않는 문제 해결
    - callback 함수는 jwt secret가 유효하지 않을 때 에러 핸들링해줘야 해서 버릴 수 없었음
    - 그래서 위 에러 핸들링만 콜백에서 처리하고, user 테이블을 조회한 후 에러 핸들링하는 부분은 콜백 밖으로 빼냄

--- server/src/utils/jwt-utils.js
+++ server/src/utils/jwt-utils.js
@@ -18,23 +18,24 @@ const generateToken = async (user) => {
   return jwtToken;
 };
 
-const decodeToken = (token) => {
-  jwt.verify(token, jwtConfig.jwtSecretKey, async (err, decoded) => {
+const decodeToken = async (token) => {
+  const decodedToken = jwt.verify(token, jwtConfig.jwtSecretKey, (err, decoded) => {
     // jwt secret이 잘못되었다면
     if (err) {
       throw new Error('jwt secret이 잘못되었음');
     }
-    const user = await db.user.findByPk(decoded.id);
-    // 유저가 존재하지 않는다면
-    if (!user) {
-      throw new Error('decoded payload에 기재된 유저가 없음');
-    }
-    // 유저가 존재하지만 토큰값이 일치하지 않는다면
-    if (user.token !== token) {
-      throw new Error('decoded payload에 기재된 유저는 있지만, 서버에서 발행해준 jwt값과 일치하지 않음');
-    }
-    return [user, decoded];
+    return decoded;
   });
+  const user = await db.user.findByPk(decodedToken.userId);
+  // 유저가 존재하지 않는다면
+  if (!user) {
+    throw new Error('decoded payload에 기재된 유저가 없음');
+  }
+  // 유저가 존재하지만 토큰값이 일치하지 않는다면
+  if (user.token !== token) {
+    throw new Error('decoded payload에 기재된 유저는 있지만, 서버에서 발행해준 jwt값과 일치하지 않음');
+  }
+  return [user, decodedToken];
 };
 
 module.exports = {
