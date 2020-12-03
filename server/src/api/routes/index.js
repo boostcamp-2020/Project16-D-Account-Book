ed31ad9a21b57d633f50363398b296c34e93dc44
lacomaco <lacomaco122@gmail.com>
2020-12-03 14:17:21 +0900

    feat : category router 생성
    - category controller을 메인 routes에 연결하였습니다.
    - category와 관련된 모든 API는 categories로 시작하도록 설정하였습니다.

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -1,10 +1,12 @@
 const Router = require('koa-router');
 const oauthRouter = require('@routes/oauth');
 const transactionsRouter = require('@routes/transactions');
+const categoryRouter = require('@routes/category');
 
 const router = new Router();
 
 router.use('/oauth', oauthRouter.routes());
 router.use('/transactions', transactionsRouter.routes());
+router.use('/categories', categoryRouter.routes());
 
 module.exports = router;
