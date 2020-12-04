c1bc5ccb47f63d052cbbe8cea86ec933bea89933
lacomaco <lacomaco122@gmail.com>
2020-12-04 22:14:50 +0900

    feat : Root Router에 account Router 연결

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -2,11 +2,13 @@ const Router = require('koa-router');
 const oauthRouter = require('@routes/oauth');
 const transactionsRouter = require('@routes/transactions');
 const categoryRouter = require('@routes/category');
+const accountRouter = require('@routes/account');
 
 const router = new Router();
 
 router.use('/oauth', oauthRouter.routes());
 router.use('/transactions', transactionsRouter.routes());
 router.use('/categories', categoryRouter.routes());
+router.use('/accounts', accountRouter.routes());
 
 module.exports = router;
