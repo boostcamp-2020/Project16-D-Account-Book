aa4c5a211c89d0a8845f4d90eb811f614a406ca4
lacomaco <lacomaco122@gmail.com>
2020-12-05 01:50:46 +0900

    feat : 의미 없는 import 구문 삭제, 의미 없는 axios.get 호출문 삭제

--- client/src/services/account.ts
+++ client/src/services/account.ts
@@ -1,12 +1,8 @@
 import Account from '../types/account';
 import instance from '../api/axios';
-import axios, { AxiosResponse } from 'axios';
 const accountAPIAddress = {
   getAccounts: '/api/accounts',
 };
-
-axios.get('');
-
 export default {
   getAccountsById: async (id: number): Promise<Account[]> => {
     const data = await instance.get<any, Account[]>(accountAPIAddress.getAccounts, {
