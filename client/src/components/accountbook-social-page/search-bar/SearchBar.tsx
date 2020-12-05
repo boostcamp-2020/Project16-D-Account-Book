34526303224cb7136cc5b0d879b071813ef9ffca
mu1616 <mu1616@naver.com>
2020-12-05 17:08:15 +0900

    feat: SearchBar 컴포넌트 생성
    - 유저 검색에 사용할 SearchBar 컴포넌트 생성

---
+++ client/src/components/accountbook-social-page/search-bar/SearchBar.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import { LIGHT_BLUE, GRAY } from '../../../constants/color';
+import SearchIcon from './SearchIcon';
+
+const SearchBarWrapper = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+const InputText = styled.input.attrs(() => ({
+  type: 'text',
+}))`
+  width: 85%;
+  padding: 15px 10px;
+  font-size: 1rem;
+  border-radius: 5px 0px 0px 5px;
+  border: 1px solid lightgray;
+  outline: none;
+`;
+
+const SearchButton = styled.div`
+  width: 15%;
+  border: 1px solid lightgray;
+  border-left: 0px;
+  border-radius: 0px 5px 5px 0px;
+  background-color: ${GRAY};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const SearchBar = (): JSX.Element => {
+  return (
+    <SearchBarWrapper>
+      <InputText />
+      <SearchButton>
+        <SearchIcon />
+      </SearchButton>
+    </SearchBarWrapper>
+  );
+};
+
+export default SearchBar;
