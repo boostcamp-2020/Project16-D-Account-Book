c0c87c039f1565e92af6a3e283e0909c1b1a23c0
lacomaco <lacomaco122@gmail.com>
2020-12-05 23:11:57 +0900

    feat : create form과 store 연결
    - createTransactionFormStore의 상태를 관찰하여 모달을 띄우고 사라지게
      하는 기능을 넣었습니다.
    
    - 생성한 categoryStore와 accountStore의 데이터가 inputs에 들어가도록
      설정하였습니다.

--- client/src/components/common/modals/form-modal-transaction/FormModalCreateTransaction.tsx
+++ client/src/components/common/modals/form-modal-transaction/FormModalCreateTransaction.tsx
@@ -4,24 +4,35 @@ import UseTransactionForm from '../../../../hook/use-transaction-form/useTransac
 import FormModalWrapper from '../form-modal-template/FormModalWrapper';
 import ModalBackground from '../modal-background/ModalBackground';
 import FormModalHeader from '../form-modal-header/FormModalHeader';
+import useStore from '../../../../hook/use-store/useStore';
 
-//임시용
-import dummyOptions from '../../../../__dummy-data__/components/inputs/dummyOptions';
+import { observer } from 'mobx-react';
+import { Options } from '../../../../types/options';
+import RootStore from '../../../../store/RootStore';
 
 const FormModalTransaction: React.FC = () => {
+  const { rootStore } = useStore();
+  const toggle = rootStore.modalStore.createTransactionFormStore;
+
   // 임시 카테고리
-  const category = dummyOptions;
+  const incomeCategory = rootStore.categoryStore.incomeOptions;
+  const expenditureCategory = rootStore.categoryStore.expenditureOptions;
   // 임시 accounts
-  const accounts = dummyOptions;
+  const accounts = rootStore.accountStore.accountOptions;
 
   //임시로 빈 객체를 넣음
   const [inputs, changes] = UseTransactionForm();
+
+  const { show } = toggle;
+  const modalToggle = (): void => {
+    toggle.toggleShow();
+  };
 
   const inputListInputs = {
     ...inputs,
     categories: {
       placeholder: '카테고리',
-      items: category,
+      items: incomeCategory,
       selected: inputs.categories,
     },
     accounts: {
@@ -31,14 +42,18 @@ const FormModalTransaction: React.FC = () => {
     },
   };
 
+  if (!show) {
+    return null;
+  }
+
   return (
-    <ModalBackground show={true}>
+    <ModalBackground show={show} closeModal={modalToggle}>
       <FormModalWrapper>
-        <FormModalHeader modalName={'내역작성'} blueName={'완료'} />
+        <FormModalHeader modalName={'내역작성'} blueName={'완료'} closeModal={modalToggle} />
         <TransactionInputList inputs={inputListInputs} changes={changes} />
       </FormModalWrapper>
     </ModalBackground>
   );
 };
 
-export default FormModalTransaction;
+export default observer(FormModalTransaction);
