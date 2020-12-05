c2af8be0302814a56e586b6f2263f0fd54a1b874
mu1616 <mu1616@naver.com>
2020-12-05 14:45:22 +0900

    refactor: InputText 컴포넌트 css
    - focus css 추가

--- client/src/constants/color.ts
+++ client/src/constants/color.ts
@@ -1,4 +1,5 @@
 export const BLUE = '#1EA7FD';
+export const LIGHT_BLUE = '#b3d1f3';
 export const RED = '#F35454';
 export const GRAY = '#E8E8E8';
 export const DEEP_GRAY = '#7f7f7f';
@@ -8,3 +9,19 @@ export const MODAL_RED = '#E74C3C';
 export const MODAL_WHITE = '#ECECEC';
 export const DODGER_BLUE = '#1E90FF';
 export const LIGHT_GREEN = '#11CD6E';
+
+const color = {
+  BLUE,
+  LIGHT_BLUE,
+  RED,
+  GRAY,
+  DEEP_GRAY,
+  LIGHT_GRAY,
+  MODAL_GRAY,
+  MODAL_RED,
+  MODAL_WHITE,
+  DODGER_BLUE,
+  LIGHT_GREEN,
+};
+
+export default color;
