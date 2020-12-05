3437a5d87dd1c689d0619bb8d4512c432d795f5d
lacomaco <lacomaco122@gmail.com>
2020-12-05 23:17:05 +0900

    feat : form을 띄우고 지우는 store 생성

---
+++ client/src/store/modal-store/CreateTransactionFormStore.tsx
@@ -0,0 +1,30 @@
+import { observable, action, makeAutoObservable } from 'mobx';
+import RootStore from '../RootStore';
+
+export default class CreateTransactionFormStore {
+  rootStore;
+
+  @observable
+  show = false;
+
+  constructor(rootStore: RootStore) {
+    this.rootStore = rootStore;
+
+    makeAutoObservable(this);
+  }
+
+  @action
+  toggleShow = (): void => {
+    this.show = !this.show;
+  };
+
+  @action
+  setShowTrue = (): void => {
+    this.show = true;
+  };
+
+  @action
+  setShowFalse = (): void => {
+    this.show = false;
+  };
+}
