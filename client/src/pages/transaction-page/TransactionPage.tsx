bee2162dee688d34f8f4d00c6f9626d15ef0a4b7
lacomaco <lacomaco122@gmail.com>
2020-12-05 23:13:35 +0900

    feat : Transaction page 로드시 데이터 받는 기능 추가
    - Transaction page가 렌더링될때 서버로부터 category 데이터와 account
      데이터를 받도록 조정하였습니다.

--- client/src/pages/transaction-page/TransactionPage.tsx
+++ client/src/pages/transaction-page/TransactionPage.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { match } from 'react-router-dom';
 import { Location } from 'history';
 import TransactionView from './TransactionView';
 import styled from 'styled-components';
 import queryString from 'query-string';
+import useStore from '../../hook/use-store/useStore';
 
 interface Props {
   match: match<{ id: string }>;
@@ -17,8 +18,17 @@ const PageWrapper = styled.div`
 interface Query {
   category: string | string[] | null;
 }
+
 const TransactionPage: React.FC<Props> = ({ match, location }: Props) => {
+  const { rootStore } = useStore();
   const query = location.search ? queryString.parse(location.search) : null;
+
+  useEffect(() => {
+    rootStore.accountStore.updateAccounts(Number(match.params.id));
+    rootStore.categoryStore.updateIncomeCategories(Number(match.params.id));
+    rootStore.categoryStore.updateExpenditureCategories(Number(match.params.id));
+  }, []);
+
   return (
     <PageWrapper>
       <TransactionView accountbookId={Number(match.params.id)} query={query} />
