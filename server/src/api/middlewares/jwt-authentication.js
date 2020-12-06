fbd60878199e306f128e67462d4cb57d6c2598b0
Mong-Gu <yhw8258@naver.com>
2020-12-07 01:01:06 +0900

    fix: 새로 jwt가 발급될 때 db 업데이트하지 않는 문제 수정

--- server/src/api/middlewares/jwt-authentication.js
+++ server/src/api/middlewares/jwt-authentication.js
@@ -1,5 +1,6 @@
 const { generateToken, decodeToken } = require('@utils/jwt-utils');
 const jwtConfig = require('@config/jwt');
+const db = require('@models');
 
 module.exports = async (ctx, next) => {
   const token = ctx.cookies.get('jwt');
@@ -7,15 +8,15 @@ module.exports = async (ctx, next) => {
     if (!token) {
       throw new Error('jwt토큰 없음');
     }
-    const [user, decoded] = await decodeToken(token);
-    if (Date.now() / 1000 - decoded.iat > 60 * 60 * 2) {
+    const decoded = await decodeToken(token);
+    if (Date.now() / 1000 - decoded.iat > 60 * 60 * 1) {
       const newToken = generateToken(decoded);
       ctx.cookies.set('jwt', newToken, {
         httpOnly: true,
         maxAge: jwtConfig.cookieExpiresIn,
       });
+      await db.user.update({ token: newToken }, { where: { id: decoded.id } });
     }
-    ctx.state.user = user;
   } catch (err) {
     ctx.throw(401, err);
   }
