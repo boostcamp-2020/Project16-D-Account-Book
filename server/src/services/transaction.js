3f33b1b4dc41402b64b2ce10fa857c0e93bcfca9
lacomaco <lacomaco122@gmail.com>
2020-12-06 13:15:23 +0900

    feat : income 내역을 DB에 저장하는 서비스 생성
    - 인자로 들어온 icome 정보를 바탕으로 income 거래내역을 생성하고
    - 생성된 거래내역을 다시 DB에서 조회해서 가져와 리턴하는 구조로
      되어있습니다.

--- server/src/services/transaction.js
+++ server/src/services/transaction.js
@@ -1,5 +1,6 @@
 const db = require('@models');
 const { Op } = require('sequelize');
+const { getAccountbookById } = require('@services/accountbook');
 
 const findIncomes = async (accountbookId, startDate, endDate) => {
   const incomes = await db.income.findAll({
@@ -53,7 +54,41 @@ const findExpenditures = async (accountbookId, startDate, endDate) => {
   return expenditures;
 };
 
+const createIncome = async ({ accountbookId, incomeCategoryId, accountId, amount, content, date, memo }) => {
+  const accountbook = await getAccountbookById(accountbookId);
+
+  const income = await accountbook.createIncome({
+    amount,
+    content,
+    date,
+    memo,
+    incomeCategoryId,
+    accountId,
+  });
+
+  const createdIncome = await db.income.findOne({
+    attributes: ['id', 'amount', 'content', 'date', 'memo'],
+    where: {
+      id: income.id,
+    },
+    include: [
+      {
+        model: db.incomeCategory,
+        as: 'category',
+        attributes: ['id', 'name', 'color'],
+      },
+      {
+        model: db.account,
+        attributes: ['id', 'name', 'color'],
+      },
+    ],
+  });
+
+  return createdIncome;
+};
+
 module.exports = {
   findIncomes,
   findExpenditures,
+  createIncome,
 };
