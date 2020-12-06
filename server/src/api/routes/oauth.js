083e5916ef8f98d163e35ab1738bad337c8132f3
Mong-Gu <yhw8258@naver.com>
2020-12-07 01:41:30 +0900

    fix: logout api가 하단부에 있으면 자꾸 에러걸리는 것 처리
    - logout 라우터가 로그인하는 api보다 아래에 있을 경우 자꾸 controller의 getConfig에서 에러를 throw
    - 위 문제를 해결하기 위해 로그아웃 api를 가장 위로 두었음
    - 이건 해결하긴 했으나 아직도 왜 그런 문제가 발생하는지 잘 모르겠습니다

--- server/src/api/routes/oauth.js
+++ server/src/api/routes/oauth.js
@@ -4,8 +4,8 @@ const router = new Router();
 
 const oauthController = require('@controllers/oauth');
 
+router.get('/logout', oauthController.logout);
 router.get('/:provider', oauthController.redirectToOauthLoginPage);
 router.get('/callback/:provider', oauthController.login);
-router.get('/logout', oauthController.logout);
 
 module.exports = router;
