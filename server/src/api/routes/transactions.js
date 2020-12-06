54f90536682128708c39e3d0fa760e276d0f3884
lacomaco <lacomaco122@gmail.com>
2020-12-06 13:14:16 +0900

    feat : income 생성 API
    - post 요청으로 income 거래내역을 생성하는 API를 만들었습니다.
    - 자세한 사항은 API Docs에 올려놓겟습니다.

--- server/src/api/routes/transactions.js
+++ server/src/api/routes/transactions.js
@@ -4,5 +4,6 @@ const transactionController = require('@controllers/transaction');
 const router = new Router();
 
 router.get('/', transactionController.findTransactions);
+router.post('/income', transactionController.createIncomeTransaction);
 
 module.exports = router;
