313f5c6f054ebf3a10000d8de0bf6c4e9b81496f
Mong-Gu <yhw8258@naver.com>
2020-12-07 00:30:00 +0900

    feat: accountbook router 추가

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -2,11 +2,13 @@ const Router = require('koa-router');
 const oauthRouter = require('@routes/oauth');
 const transactionRouter = require('@routes/transaction');
 const categoryRouter = require('@routes/category');
+const accountbookRouter = require('@routes/accountbook');
 
 const router = new Router();
 
 router.use('/oauth', oauthRouter.routes());
 router.use('/transactions', transactionRouter.routes());
 router.use('/categories', categoryRouter.routes());
+router.use('/accountbooks', accountbookRouter.routes());
 
 module.exports = router;
