42fabc8a4ae6bddb1bea874ba9803b905d74064c
Mong-Gu <yhw8258@naver.com>
2020-12-07 00:24:36 +0900

    feat: logout api 엔드포인트 추가

--- server/src/api/routes/oauth.js
+++ server/src/api/routes/oauth.js
@@ -5,7 +5,7 @@ const router = new Router();
 const oauthController = require('@controllers/oauth');
 
 router.get('/:provider', oauthController.redirectToOauthLoginPage);
-
 router.get('/callback/:provider', oauthController.login);
+router.get('/logout', oauthController.logout);
 
 module.exports = router;
