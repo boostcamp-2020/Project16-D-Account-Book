bd76d6ff395868a38f5eb5834452743603e4f8ee
Mong-Gu <yhw8258@naver.com>
2020-12-07 01:12:24 +0900

    fix: accountbook service쪽으로 옮겨야 될 함수 이동

--- server/src/services/category.js
+++ server/src/services/category.js
@@ -1,18 +1,4 @@
-const db = require('@models');
-
-const getAccountbookById = async (id) => {
-  const accountbook = await db.accountbook.findOne({
-    where: {
-      id,
-    },
-  });
-
-  if (accountbook === null) {
-    throw new Error('accountbook이 존재하지 않습니다.');
-  }
-
-  return accountbook;
-};
+const { getAccountbookById } = require('@services/accountbook');
 
 const getIncomeCategoriesByAccountbookId = async (id) => {
   const accountbook = await getAccountbookById(id);
