4335c688d87b3eec11c7ddbe3d9e7cd58e96a7c1
Mong-Gu <yhw8258@naver.com>
2020-12-07 00:29:27 +0900

    style: 변수명을 복수형에서 단수형으로 변경

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -1,12 +1,12 @@
 const Router = require('koa-router');
 const oauthRouter = require('@routes/oauth');
-const transactionsRouter = require('@routes/transactions');
+const transactionRouter = require('@routes/transaction');
 const categoryRouter = require('@routes/category');
 
 const router = new Router();
 
 router.use('/oauth', oauthRouter.routes());
-router.use('/transactions', transactionsRouter.routes());
+router.use('/transactions', transactionRouter.routes());
 router.use('/categories', categoryRouter.routes());
 
 module.exports = router;
