dd1cf14a967767819c7ee18d1247ef36647d0d18
mu1616 <mu1616@naver.com>
2020-12-08 08:20:16 +0900

    feaet: 지출 수정 api 라우팅 설정

--- server/src/api/routes/transaction.js
+++ server/src/api/routes/transaction.js
@@ -7,4 +7,5 @@ router.get('/', transactionController.findTransactions);
 router.post('/income', transactionController.createIncomeTransaction);
 router.post('/expenditure', transactionController.createExpenditureTransaction);
 router.patch('/income/:id', transactionController.updateIncomeTransaction);
+router.patch('/expenditure/:id', transactionController.updateExpenditureTransaction);
 module.exports = router;
