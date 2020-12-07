e69166a2d57550a0cc1d9f48a359dd79fc9cb7e9
bbbyung2 <bhko0524@naver.com>
2020-12-07 22:51:54 +0900

    feat: account 생성 controller 추가 및 camelCase 리팩토링
    - createAccount 함수 구현
    - getAccounts 함수에서 accountbook_id를 accountbookId로 변경

--- server/src/api/controllers/account.js
+++ server/src/api/controllers/account.js
@@ -1,18 +1,26 @@
 const accountService = require('@services/account');
 
-const validateAccountBookQuery = (accountbook_id) => {
-  if (accountbook_id === undefined) {
+const validateAccountBookQuery = (accountbookId) => {
+  if (accountbookId === undefined) {
     throw new Error('accountbook id를 반드시 포함시켜야합니다.');
   }
 };
 
 const getAccounts = async (ctx) => {
-  const { accountbook_id } = ctx.request.query;
-  validateAccountBookQuery(accountbook_id);
-  const accounts = await accountService.getAccountsByAccountbookId(accountbook_id);
+  const { accountbook_id: accountbookId } = ctx.request.query;
+  validateAccountBookQuery(accountbookId);
+  const accounts = await accountService.getAccountsByAccountbookId(accountbookId);
   ctx.body = accounts;
+};
+
+const createAccount = async (ctx) => {
+  const { id: accountbookId } = ctx.request.query;
+  const accountData = ctx.request.body;
+  const createdAccount = await accountService.createAccount(accountbookId, accountData);
+  ctx.body = createdAccount;
 };
 
 module.exports = {
   getAccounts,
+  createAccount,
 };
