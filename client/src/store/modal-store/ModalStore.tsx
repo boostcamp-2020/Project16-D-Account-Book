b40e132c5a7147cd2875383900e21219de0f1563
lacomaco <lacomaco122@gmail.com>
2020-12-07 16:22:13 +0900

    feat : 내역 모달창과 모달 스토어 연결

--- client/src/store/modal-store/ModalStore.tsx
+++ client/src/store/modal-store/ModalStore.tsx
@@ -1,14 +1,16 @@
 import FormFilterStore from './FormFilterStore';
 import RootStore from '../RootStore';
 import CreateTransactionFormStore from './CreateTransactionFormStore';
+import UpdateTransactionFormStore from './UpdateTransactionFormStore';
 export default class ModalStore {
   rootStore: RootStore;
   formFilterStore: FormFilterStore;
   createTransactionFormStore: CreateTransactionFormStore;
-
+  updateTransactionFormStore: UpdateTransactionFormStore;
   constructor(rootStore: RootStore) {
     this.formFilterStore = new FormFilterStore(rootStore);
     this.rootStore = rootStore;
     this.createTransactionFormStore = new CreateTransactionFormStore(rootStore);
+    this.updateTransactionFormStore = new UpdateTransactionFormStore(rootStore);
   }
 }
