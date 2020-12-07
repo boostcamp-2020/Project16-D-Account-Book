9f636950323399ee50c14828b4782c10cd9611ad
bbbyung2 <bhko0524@naver.com>
2020-12-08 01:28:21 +0900

    feat: FormModalCreateAccount 컴포넌트 스토리 파일 추가

--- client/src/components/common/modals/modals.stories.tsx
+++ client/src/components/common/modals/modals.stories.tsx
@@ -8,6 +8,7 @@ import FormModalFilter from './form-modal-filter/FormModalFilter';
 import { inputs, changes } from '../../../__dummy-data__/components/modal/modalTransactions';
 import { inputs as filterInputs, changes as filterChanges } from '../../../__dummy-data__/components/modal/modalFilter';
 import FormModalTransaction from './form-modal-transaction/FormModalCreateTransaction';
+import FormModalAccount from './form-modal-account/FormModalCreateAccount';
 import styled from 'styled-components';
 
 export default {
@@ -70,4 +71,8 @@ export const FormModalFilterDefault: React.FC = () => {
 
 export const FormModalTransactionDefault: React.FC = () => {
   return <FormModalTransaction />;
+};
+
+export const FormModalAccountDefault: React.FC = () => {
+  return <FormModalAccount />;
 };
