233278103fd2d930c8fa72982414c23506f885ef
Mong-Gu <yhw8258@naver.com>
2020-12-07 23:45:36 +0900

    refactor: jwt를 쿠키에 넣는 것으로 변경함에 따라 request 관련 설정 제거

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -4,17 +4,6 @@ const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
 
-instance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
-
 instance.interceptors.response.use(
   (response) => {
     return response.data;
