2142942c3b7344aacbcd2636800a2dece6da49a2
bbbyung2 <bhko0524@naver.com>
2020-12-07 23:25:53 +0900

    feat: 지출 카테고리 생성 router 추가

--- server/src/api/routes/category.js
+++ server/src/api/routes/category.js
@@ -6,5 +6,6 @@ const router = new Router();
 router.get('/income', categoryController.getIncomeCategories);
 router.get('/expenditure', categoryController.getExpenditureCategories);
 router.post('/income', categoryController.createIncomeCategory);
+router.post('/expenditure', categoryController.createExpenditureCategory);
 
 module.exports = router;
