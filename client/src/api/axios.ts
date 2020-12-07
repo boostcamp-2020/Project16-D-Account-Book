43bb8c1f587ce979d3e222a6a72e25b142668e8a
Mong-Gu <yhw8258@naver.com>
2020-12-08 00:23:41 +0900

    refcator: response의 status를 써야하기에 수정
    rest api 원칙에 따라 response.status를 사용하여 상태코드를 이용해야 함

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -4,14 +4,4 @@ const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
 
-instance.interceptors.response.use(
-  (response) => {
-    return response.data;
-  },
-  (error) => {
-    console.error(error);
-    return Promise.reject(error);
-  },
-);
-
 export default instance;
