b32e7df0969ba109a4cdadf4f8a655da4183f457
bbbyung2 <bhko0524@naver.com>
2020-12-07 15:23:56 +0900

    feat: modalStore에 createCategoryStore 추가

--- client/src/store/modal-store/ModalStore.tsx
+++ client/src/store/modal-store/ModalStore.tsx
@@ -1,11 +1,14 @@
+import CreateCategoryStore from './CreateCategoryStore';
 import FormFilterStore from './FormFilterStore';
 import RootStore from '../RootStore';
 
 export default class ModalStore {
   rootStore;
   formFilterStore;
+  createCategoryStore;
 
   constructor(rootStore: RootStore) {
+    this.createCategoryStore = new CreateCategoryStore(rootStore);
     this.formFilterStore = new FormFilterStore(rootStore);
     this.rootStore = rootStore;
   }
