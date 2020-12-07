15f5f089431cc90a3a55e8bc3de8e20b766b3deb
bbbyung2 <bhko0524@naver.com>
2020-12-08 02:46:23 +0900

    feat: 카테고리 생성 모달창 구현

---
+++ client/src/components/common/modals/form-modal-category/FormModalCreateCategory.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import FormModalWrapper from '../form-modal-template/FormModalWrapper';
+import FormModalItem from '../form-modal-template/FormModalItemWrapper';
+import FormModalLabel from '../form-modal-template/FormModalLabel';
+import ModalBackground from '../modal-background/ModalBackground';
+import FormModalHeader from '../form-modal-header/FormModalHeader';
+import { observer } from 'mobx-react';
+import CategoryPreview from '../../category-preview/CategoryPreview';
+import InputText from '../../inputs/input-text/InputText';
+
+const FormModalCategory: React.FC = () => {
+  const [name, setName] = useState<string>('카테고리 1');
+  const [inputColor, setInputColor] = useState<string>('black');
+
+  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const onChange = (color: { hex: string }): void => {
+    setInputColor(color.hex);
+  };
+
+  return (
+    <ModalBackground show={true}>
+      <FormModalWrapper>
+        <FormModalHeader modalName={'카테고리 생성'} blueName={'생성'} />
+        <FormModalItem>
+          <CategoryPreview title={name} color={inputColor} onChange={onChange} />
+        </FormModalItem>
+        <FormModalItem>
+          <FormModalLabel>카테고리 이름</FormModalLabel>
+          <InputText
+            maxLength={8}
+            placeholder={'최대 8자의 카테고리명을 입력해주세요.'}
+            value={name}
+            onChange={onChangeName}
+          />
+        </FormModalItem>
+      </FormModalWrapper>
+    </ModalBackground>
+  );
+};
+
+export default observer(FormModalCategory);
