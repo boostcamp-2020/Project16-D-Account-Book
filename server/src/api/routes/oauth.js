26a912b76cedff1df51b216168d9c4ec4a101e5d
Mong-Gu <yhw8258@naver.com>
2020-12-07 11:15:56 +0900

    fix: 엔드포인트 uri 수정

--- server/src/api/routes/oauth.js
+++ server/src/api/routes/oauth.js
@@ -5,7 +5,7 @@ const router = new Router();
 const oauthController = require('@controllers/oauth');
 
 router.get('/logout', oauthController.logout);
-router.get('/:provider', oauthController.redirectToOauthLoginPage);
+router.get('/login/:provider', oauthController.redirectToOauthLoginPage);
 router.get('/callback/:provider', oauthController.login);
 
 module.exports = router;
