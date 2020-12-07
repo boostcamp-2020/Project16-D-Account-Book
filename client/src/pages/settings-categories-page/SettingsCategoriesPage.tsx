e965995f6c176f85294d688fac5fe07e9499e6d6
bbbyung2 <bhko0524@naver.com>
2020-12-07 17:25:05 +0900

    chore: 사용하지 않는 useState 삭제

--- client/src/pages/settings-categories-page/SettingsCategoriesPage.tsx
+++ client/src/pages/settings-categories-page/SettingsCategoriesPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { match } from 'react-router-dom';
 import styled from 'styled-components';
 import SettingsSidebar from '../../components/common/settings-sidebar/SettingsSidebar';
