aced98ae1dec9d8f9305aa6d70c58c554904c108
mu1616 <mu1616@naver.com>
2020-12-08 15:19:21 +0900

    feat: 유저검색 api 라우팅 설정

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -4,6 +4,7 @@ const transactionRouter = require('@routes/transaction');
 const categoryRouter = require('@routes/category');
 const accountRouter = require('@routes/account');
 const accountbookRouter = require('@routes/accountbook');
+const socialRouter = require('@routes/social');
 
 const router = new Router();
 
@@ -12,5 +13,6 @@ router.use('/transactions', transactionRouter.routes());
 router.use('/categories', categoryRouter.routes());
 router.use('/accounts', accountRouter.routes());
 router.use('/accountbooks', accountbookRouter.routes());
+router.use('/social', socialRouter.routes());
 
 module.exports = router;
---
+++ server/src/api/routes/social.js
@@ -0,0 +1,15 @@
+const Router = require('koa-router');
+// const transactionController = require('@controllers/transaction');
+const socialController = require('@controllers/social');
+
+const router = new Router();
+
+router.get('/', socialController.searchUser);
+// router.get('/', transactionController.findTransactions);
+// router.post('/income', transactionController.createIncomeTransaction);
+// router.post('/expenditure', transactionController.createExpenditureTransaction);
+// router.patch('/income/:id', transactionController.updateIncomeTransaction);
+// router.patch('/expenditure/:id', transactionController.updateExpenditureTransaction);
+// router.delete('/income/:id', transactionController.deleteIncomeTransaction);
+// router.delete('/expenditure/:id', transactionController.deleteExpenditureTransaction);
+module.exports = router;
