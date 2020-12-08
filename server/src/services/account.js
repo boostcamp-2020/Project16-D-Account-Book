4bab5e66ac6e492c47ecca93c6970371f03f6858
bbbyung2 <bhko0524@naver.com>
2020-12-08 22:21:00 +0900

    feat: account의 update 후 인스턴스 반환을 위한 findAccountById 함수 구현

--- server/src/services/account.js
+++ server/src/services/account.js
@@ -9,18 +9,27 @@ const getAccountsByAccountbookId = async (id) => {
   return accounts;
 };
 
+const findAccountById = async (id) => {
+  const account = await db.account.findOne({
+    attributes: ['id', 'name', 'color'],
+    where: {
+      id,
+    },
+  });
+  return account;
+};
+
 const createAccount = async ({ accountbookId, name, color }) => {
   const accountbook = await getAccountbookById(accountbookId);
-
   const createdAccount = await accountbook.createAccount({
     name,
     color,
   });
-
   return createdAccount;
 };
 
 module.exports = {
   getAccountsByAccountbookId,
   createAccount,
+  findAccountById,
 };
