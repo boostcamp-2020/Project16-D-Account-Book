d34455b2a3395935dd219edabc971defce4b7df9
bbbyung2 <bhko0524@naver.com>
2020-12-09 02:02:57 +0900

    feat: UpdateCategoryFormStore 구현

---
+++ client/src/store/modal-store/UpdateCategoryFormStore.tsx
@@ -0,0 +1,29 @@
+import { observable, action, makeAutoObservable } from 'mobx';
+import RootStore from '../RootStore';
+
+export default class UpdateCategoryFormStore {
+  rootStore: RootStore;
+
+  @observable
+  show = false;
+
+  constructor(rootStore: RootStore) {
+    this.rootStore = rootStore;
+    makeAutoObservable(this);
+  }
+
+  @action
+  toggleShow = (): void => {
+    this.show = !this.show;
+  };
+
+  @action
+  setShowTrue = (): void => {
+    this.show = true;
+  };
+
+  @action
+  setShowFalse = (): void => {
+    this.show = false;
+  };
+}
