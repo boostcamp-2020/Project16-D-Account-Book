0294649bcc25f82a8740416a369f54171bded328
mu1616 <mu1616@naver.com>
2020-12-08 15:48:47 +0900

    feat: 소셜페이지 라우팅 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import SettingsAccountbookPage from './pages/settings-accountbook-page/SettingsA
 import SettingsCategoriesPage from './pages/settings-categories-page/SettingsCategoriesPage';
 import SettingsAccountsPage from './pages/settings-accounts-page/SettingsAccountsPage';
 import SettingsCsvPage from './pages/settings-csv-page/SettingsCsvPage';
+import SettingsSocialPage from './pages/settings-social-page/SettingsSocialPage';
 
 function App(): JSX.Element {
   return (
@@ -22,6 +23,7 @@ function App(): JSX.Element {
             <Route exact path="/accountbooks/:id/settings/categories" component={SettingsCategoriesPage} />
             <Route exact path="/accountbooks/:id/settings/accounts" component={SettingsAccountsPage} />
             <Route exact path="/accountbooks/:id/settings/csv" component={SettingsCsvPage} />
+            <Route exact path="/accountbooks/:id/settings/social" component={SettingsSocialPage} />
           </Switch>
         </Router>
       </RootProvider>
