f7717846b6faa5fbde339b5471f9e7ade4d0570e
bbbyung2 <bhko0524@naver.com>
2020-12-08 11:51:35 +0900

    refactor: 최상단의 RootProvider와 중복되는 문제 해결
    - 스토리 파일에서 RootProvider 삭제

--- client/src/pages/settings-accounts-page/SettingsAccountsView.stories.tsx
+++ client/src/pages/settings-accounts-page/SettingsAccountsView.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import SettingsAccountsView from './SettingsAccountsView';
-import { RootProvider } from '../../store/RootStore';
 
 export default {
   title: 'pages/settings-page/SettingsAccountsPage',
@@ -8,9 +7,5 @@ export default {
 };
 
 export const Default = (): JSX.Element => {
-  return (
-    <RootProvider>
-      <SettingsAccountsView accountbookId={1} />
-    </RootProvider>
-  );
+  return <SettingsAccountsView accountbookId={1} />;
 };
--- client/src/pages/settings-categories-page/SettingsCategoriesView.stories.tsx
+++ client/src/pages/settings-categories-page/SettingsCategoriesView.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import SettingsCategoriesView from './SettingsCategoriesView';
-import { RootProvider } from '../../store/RootStore';
 
 export default {
   title: 'pages/settings-page/SettingsCategoriesPage',
@@ -8,9 +7,5 @@ export default {
 };
 
 export const Default: React.FC = () => {
-  return (
-    <RootProvider>
-      <SettingsCategoriesView accountbookId={1} />
-    </RootProvider>
-  );
+  return <SettingsCategoriesView accountbookId={1} />;
 };
