66c4551a72da96f100738467cda7059173c15d46
bbbyung2 <bhko0524@naver.com>
2020-12-08 11:20:33 +0900

    feat: ModalStore에 createAccountFormStore, createCategoryFormStore 추가

--- client/src/store/modal-store/ModalStore.tsx
+++ client/src/store/modal-store/ModalStore.tsx
@@ -1,17 +1,21 @@
-// import CreateCategoryStore from './CreateCategoryStore';
 import FormFilterStore from './FormFilterStore';
 import RootStore from '../RootStore';
 import CreateTransactionFormStore from './CreateTransactionFormStore';
+import CreateAccountFormStore from './CreateAccountFormStore';
+import CreateCategoryFormStore from './CreateCategoryFormStore';
+
 export default class ModalStore {
   rootStore: RootStore;
   formFilterStore: FormFilterStore;
   createTransactionFormStore: CreateTransactionFormStore;
-  // createCategoryStore: CreateCategoryStore;
+  createAccountFormStore: CreateAccountFormStore;
+  createCategoryFormStore: CreateCategoryFormStore;
 
   constructor(rootStore: RootStore) {
-    // this.createCategoryStore = new CreateCategoryStore(rootStore);
     this.formFilterStore = new FormFilterStore(rootStore);
     this.rootStore = rootStore;
     this.createTransactionFormStore = new CreateTransactionFormStore(rootStore);
+    this.createAccountFormStore = new CreateAccountFormStore(rootStore);
+    this.createCategoryFormStore = new CreateCategoryFormStore(rootStore);
   }
 }
