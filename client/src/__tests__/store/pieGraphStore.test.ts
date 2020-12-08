efa56d5acf6178938be46068520c6d4b4f661286
lacomaco <lacomaco122@gmail.com>
2020-12-08 21:26:58 +0900

    feat : pieGraphStore 테스트 뼈대 생성

---
+++ client/src/__tests__/store/pieGraphStore.test.ts
@@ -0,0 +1,9 @@
+import RootStore from '../../store/RootStore';
+
+describe('PieGraphStore Test', () => {
+  beforeEach(() => {
+    const rootStore = new RootStore();
+    const { pieGraphPageStore } = rootStore;
+  });
+  test('for Pass', () => {});
+});
