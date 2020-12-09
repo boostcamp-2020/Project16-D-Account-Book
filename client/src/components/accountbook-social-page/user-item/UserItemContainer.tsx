f5f198ebd7ec3152ae37e09fa4391f99b77b6489
mu1616 <mu1616@naver.com>
2020-12-09 10:44:05 +0900

    refactor: UserItemContainer 리팩토링
    - 유저/어드민을 구분해서 렌더링했었는데 버튼타입만 admin/user에 따라
    분기하도록 수정

--- client/src/components/accountbook-social-page/user-item/UserItemContainer.tsx
+++ client/src/components/accountbook-social-page/user-item/UserItemContainer.tsx
@@ -12,31 +12,18 @@ const Container = styled.div`
 
 const UserItemContainer = (): JSX.Element => {
   const { socialStore } = useStore().rootStore;
-  const userAccountbooks = socialStore.userAccountbooks;
-  const admin = userAccountbooks[0];
 
   return (
     <Container>
       <h3>가계부 구성원 목록</h3>
-      {admin && (
-        <UserItem
-          id={admin.id}
-          key={admin.user.email}
-          email={admin.user.email}
-          profileUrl={admin.user.profileUrl}
-          nickname={admin.user.nickname}
-          type="admin"
-        />
-      )}
-
-      {socialStore.userAccountbooks?.slice(1).map((userAccountbook) => (
+      {socialStore.userAccountbooks?.map((userAccountbook) => (
         <UserItem
           id={userAccountbook.user.id}
           key={userAccountbook.user.email}
           email={userAccountbook.user.email}
           profileUrl={userAccountbook.user.profileUrl}
           nickname={userAccountbook.user.nickname}
-          type="user"
+          type={userAccountbook.authority ? 'admin' : 'user'}
         />
       ))}
     </Container>
