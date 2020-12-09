3455c205ee5bafb2cbfc68b7b6aa3b7c74941ec5
Mong-Gu <yhw8258@naver.com>
2020-12-10 02:00:36 +0900

    refactor: 다른 함수들과 error throw하는 부분 통일
    try-catch문으로 error를 잡은 후 ctx.throw로 상태코드와 에러메세지를 반환하도록 수정

--- server/src/api/controllers/auth.js
+++ server/src/api/controllers/auth.js
@@ -4,21 +4,23 @@ const db = require('@models');
 
 const getCurrentUser = async (ctx) => {
   const token = ctx.cookies.get('jwt');
-  if (!token) {
-    const error = new Error('jwt토큰 없음');
-    error.status = 401;
-    throw error;
+  try {
+    if (!token) {
+      throw new Error('jwt토큰 없음');
+    }
+    const [decoded, user] = await decodeTokenForValidation(token);
+    if (60 * 60 * 2 > Date.now() / 1000 - decoded.iat > 60 * 60 * 1) {
+      const newToken = await generateToken(decoded.userId);
+      ctx.cookies.set('jwt', newToken, {
+        httpOnly: true,
+        maxAge: jwtConfig.cookieExpiresIn,
+      });
+      await db.user.update({ token: newToken }, { where: { id: decoded.userId } });
+    }
+    ctx.body = user;
+  } catch (err) {
+    ctx.throw(401, err);
   }
-  const [decoded, user] = await decodeTokenForValidation(token);
-  if (60 * 60 * 2 > Date.now() / 1000 - decoded.iat > 60 * 60 * 1) {
-    const newToken = await generateToken(decoded.userId);
-    ctx.cookies.set('jwt', newToken, {
-      httpOnly: true,
-      maxAge: jwtConfig.cookieExpiresIn,
-    });
-    await db.user.update({ token: newToken }, { where: { id: decoded.userId } });
-  }
-  ctx.body = user;
 };
 
 module.exports = {
