d8375d0a563fe7d1811a0730a2a5548660006151
lacomaco <lacomaco122@gmail.com>
2020-12-09 22:12:02 +0900

    feat : PieGraphPageStore 기능 추가
    - 시작일, 끝일이 변경되면 항상 새로운 데이터를 가져오도록 액션을
      수정하였습니다.
    - 거래내역이 변경되면 박스차트와 파이차트에서 쓰이는 데이터또한
      변경되도록 computed 속성을 추가하였습니다.

--- client/src/store/PieGraphPageStore.tsx
+++ client/src/store/PieGraphPageStore.tsx
@@ -4,7 +4,10 @@ import Expenditure from '../types/expenditure';
 import { dateOptions } from '../__dummy-data__/store/formFilterStore';
 import { action, makeObservable, observable, computed, flow } from 'mobx';
 import datePeriod, { datePeriodNumber } from '../constants/datePeriod';
-
+import transactionService from '../services/transaction';
+import PieChartValue from '../types/pieChartValue';
+import { getOnlyIncome, getTopFiveCategory, getOnlyExpenditure } from '../utils/filter';
+import BoxChartValue from '../types/boxChartValue';
 export default class PieGraphPageStore {
   rootStore: RootStore;
   dateOptions = dateOptions;
@@ -19,6 +22,9 @@ export default class PieGraphPageStore {
   startDate: Date;
 
   @observable
+  incomeMode = false;
+
+  @observable
   endDate: Date;
   constructor(rootStore: RootStore) {
     this.rootStore = rootStore;
@@ -30,30 +36,43 @@ export default class PieGraphPageStore {
   }
 
   @action
-  changeSelectedDate = (selectedType: string): void => {
+  changeSelectedDate = (selectedType: string, accountbookId: number): void => {
     this.selectedDate = selectedType;
-    this.startDate = this.getBeforeDateByPeriod(this.endDate, selectedType);
+    const prevDate = this.getBeforeDateByPeriod(this.endDate, selectedType);
+    this.dateChange(prevDate, this.endDate, accountbookId);
     if (selectedType === datePeriod.ALL) {
-      this.endDate = new Date();
-      this.startDate = new Date(0);
+      this.dateChange(new Date(0), new Date(), accountbookId);
     }
   };
 
   @action
-  moveToPrev = (): void => {
-    this.endDate = this.startDate;
-    this.startDate = this.getBeforeDateByPeriod(this.endDate, this.selectedDate);
+  moveToPrev = (accountbookId: number): void => {
+    const endDate = this.startDate;
+    const startDate = this.getBeforeDateByPeriod(endDate, this.selectedDate);
+    this.dateChange(startDate, endDate, accountbookId);
   };
 
   @action
-  moveToNext = (): void => {
+  moveToNext = (accountbookId: number): void => {
     const nextDate = this.getNextDateByPeriod(this.endDate, this.selectedDate);
     if (nextDate.valueOf() > new Date().valueOf()) {
       return;
     }
-    this.startDate = this.endDate;
-    this.endDate = nextDate;
+    this.dateChange(this.endDate, nextDate, accountbookId);
   };
+
+  dateChange = flow(function* (this: PieGraphPageStore, startDate: Date, endDate: Date, accountbookId: number) {
+    this.startDate = startDate;
+    this.endDate = endDate;
+    const transactions = yield transactionService.getTransactions(accountbookId, startDate, endDate);
+    this.transactions = transactions;
+  });
+
+  loadTransactions = flow(function* (this: PieGraphPageStore, accountbookId: number) {
+    const transactions = yield transactionService.getTransactions(accountbookId, this.startDate, this.endDate);
+    this.transactions = transactions;
+    console.log(transactions);
+  });
 
   getBeforeDateByPeriod = (endDate: Date, selectedType: string): Date => {
     const result = new Date(endDate.valueOf());
@@ -77,6 +96,33 @@ export default class PieGraphPageStore {
       this.startDate.getMonth() + 1
     }월 ${this.startDate.getDate()}일 ~ ${this.endDate.getFullYear()}년 ${
       this.endDate.getMonth() + 1
-    }월 ${this.endDate.getDate()}알`;
+    }월 ${this.endDate.getDate()}일`;
+  }
+
+  @computed
+  get pieChartValue(): PieChartValue[] {
+    const targetTransactions = this.incomeMode
+      ? getOnlyIncome(this.transactions)
+      : getOnlyExpenditure(this.transactions);
+
+    const topOfFive = getTopFiveCategory(targetTransactions);
+    return topOfFive;
+  }
+
+  @computed
+  get boxChartValue(): BoxChartValue[] {
+    const targetTransactions = this.incomeMode
+      ? getOnlyIncome(this.transactions)
+      : getOnlyExpenditure(this.transactions);
+
+    const topOfFive = getTopFiveCategory(targetTransactions);
+    return topOfFive;
+  }
+
+  @computed
+  get totalValue(): number {
+    return this.transactions.reduce((acc, curr) => {
+      return acc + curr.amount;
+    }, 0);
   }
 }
