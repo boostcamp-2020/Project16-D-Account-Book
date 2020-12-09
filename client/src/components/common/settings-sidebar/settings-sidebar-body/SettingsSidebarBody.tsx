105404dda54800be19a96a182e9b97de8c8f36a1
bbbyung2 <bhko0524@naver.com>
2020-12-09 18:17:07 +0900

    feat: 가계부 관리 페이지의 Sidebar를 통해 다른 설정으로 이동할 수 있는 Link 태그 추가
    - Link 태그가 a태그의 성질을 가지고 있어서 생기는 문제 StyledLink 컴포넌트를 만들어서 해결

--- client/src/components/common/settings-sidebar/settings-sidebar-body/SettingsSidebarBody.tsx
+++ client/src/components/common/settings-sidebar/settings-sidebar-body/SettingsSidebarBody.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { LIGHT_GRAY, GRAY, BLUE } from '../../../../constants/color';
+import { Link } from 'react-router-dom';
+import useGetParam from '../../../../hook/use-get-param/useGetParam';
 
 const SettingsSidebarBodyWrapper = styled.div`
   margin: 0 auto;
@@ -10,10 +12,9 @@ const SettingsSidebarBodyWrapper = styled.div`
 
 interface SettingsSidebarMenuItemProps {
   currentPage: string;
-  onClick?: void;
 }
 
-const SettingsSidebarMenuItem = styled.div<SettingsSidebarMenuItemProps>`
+const SettingsSidebarMenuItem = styled.div`
   padding: 12px 20px;
   cursor: pointer;
   font-size: 1rem;
@@ -21,6 +22,15 @@ const SettingsSidebarMenuItem = styled.div<SettingsSidebarMenuItemProps>`
   &:hover {
     background-color: ${LIGHT_GRAY};
     border: 1px solid ${GRAY};
+  }
+`;
+
+const StyledLink = styled(Link)<SettingsSidebarMenuItemProps>`
+  text-decoration: none;
+  color: inherit;
+
+  &:visited {
+    text-decoration: none;
   }
 
   &:nth-child(1) {
@@ -35,12 +45,10 @@ const SettingsSidebarMenuItem = styled.div<SettingsSidebarMenuItemProps>`
     color: ${({ currentPage }) => (currentPage == 'accounts' ? BLUE : 'black')};
     font-weight: ${({ currentPage }) => (currentPage == 'accounts' ? 'bold' : 'normal')};
   }
-
   &:nth-child(4) {
     color: ${({ currentPage }) => (currentPage == 'social' ? BLUE : 'black')};
     font-weight: ${({ currentPage }) => (currentPage == 'social' ? 'bold' : 'normal')};
   }
-
   &:nth-child(5) {
     color: ${({ currentPage }) => (currentPage == 'csv' ? BLUE : 'black')};
     font-weight: ${({ currentPage }) => (currentPage == 'csv' ? 'bold' : 'normal')};
@@ -52,13 +60,24 @@ interface SettingsSidebarBodyProps {
 }
 
 const SettingsSidebarBody = ({ currentPage }: SettingsSidebarBodyProps): JSX.Element => {
+  const id = useGetParam();
   return (
     <SettingsSidebarBodyWrapper>
-      <SettingsSidebarMenuItem currentPage={currentPage}>가계부 설정</SettingsSidebarMenuItem>
-      <SettingsSidebarMenuItem currentPage={currentPage}>카테고리 관리</SettingsSidebarMenuItem>
-      <SettingsSidebarMenuItem currentPage={currentPage}>결제수단 관리</SettingsSidebarMenuItem>
-      <SettingsSidebarMenuItem currentPage={currentPage}>소셜</SettingsSidebarMenuItem>
-      <SettingsSidebarMenuItem currentPage={currentPage}>CSV 파일 관리</SettingsSidebarMenuItem>
+      <StyledLink currentPage={currentPage} to={`/accountbooks/${id}/settings/accountbook`}>
+        <SettingsSidebarMenuItem>가계부 설정</SettingsSidebarMenuItem>
+      </StyledLink>
+      <StyledLink currentPage={currentPage} to={`/accountbooks/${id}/settings/categories`}>
+        <SettingsSidebarMenuItem>카테고리 관리</SettingsSidebarMenuItem>
+      </StyledLink>
+      <StyledLink currentPage={currentPage} to={`/accountbooks/${id}/settings/accounts`}>
+        <SettingsSidebarMenuItem>결제수단 관리</SettingsSidebarMenuItem>
+      </StyledLink>
+      <StyledLink currentPage={currentPage} to={`/accountbooks/${id}/settings/social`}>
+        <SettingsSidebarMenuItem>소셜</SettingsSidebarMenuItem>
+      </StyledLink>
+      <StyledLink currentPage={currentPage} to={`/accountbooks/${id}/settings/csv`}>
+        <SettingsSidebarMenuItem>CSV 파일 관리</SettingsSidebarMenuItem>
+      </StyledLink>
     </SettingsSidebarBodyWrapper>
   );
 };
