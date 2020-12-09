7e554e652a3079df0c7c3c9f6076250e27f96220
Mong-Gu <yhw8258@naver.com>
2020-12-09 20:40:36 +0900

    feat: axios response의 error 처리
    - 로그인하지 않은 유저가 특정 페이지에 접근했을 때, status code를 401로 반환
    - status code가 401일 때, login페이지로 redirect시키는 과정 구현

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -5,4 +5,16 @@ const instance = axios.create({
   withCredentials: true,
 });
 
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default instance;
