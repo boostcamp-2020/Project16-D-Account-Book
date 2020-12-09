c2250534dffeb52f08345cf27b01b4d2c36cc4c6
lacomaco <lacomaco122@gmail.com>
2020-12-09 23:29:50 +0900

    feat : PieGraphPage 전체에서 쓰이는 상수들을 constants 폴더에 분리하였습니다.

---
+++ client/src/constants/pieGraphPage.ts
@@ -0,0 +1,11 @@
+export const text = {
+  totalIncome: '총 수입',
+  totalExpenditure: '총 지출',
+  showIncome: '수입 통계 보기',
+  showExpenditure: '지출 통계 보기',
+  dateInputPlaceholder: '날짜를 선택해주세요',
+};
+
+export const color = {
+  naverGreen: '#2EA043',
+};
