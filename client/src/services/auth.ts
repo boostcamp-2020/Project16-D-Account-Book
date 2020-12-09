86a0dc8ccd7152725baf8edae5c025e298c5ace5
Mong-Gu <yhw8258@naver.com>
2020-12-09 20:47:01 +0900

    feat: jwt 기반으로 로그인된 유저인지 검증하는 함수 구현

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -2,11 +2,21 @@ import instance from '../api/axios';
 
 const authAPIAddress = {
   logout: '/api/oauth/logout',
+  getCurrentUser: '/api/auth/current',
 };
 
 export default {
   logout: async (): Promise<number> => {
     const response = await instance.get(authAPIAddress.logout);
     return response.status;
+  },
+
+  getCurrentUser: async (): Promise<any> => {
+    try {
+      const response = await instance.get(authAPIAddress.getCurrentUser);
+      return response.data;
+    } catch (err) {
+      console.log(err);
+    }
   },
 };
