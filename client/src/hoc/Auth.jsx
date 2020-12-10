6e3776474b08757da7e36cbf2fa3d0d3d1472932
mu1616 <mu1616@naver.com>
2020-12-10 16:28:48 +0900

    refactor: auth hoc 수정
    - 변수명 가독성 좋게 수정
    - 모든 타입이 any로 되어있어서 확장자를 jsx로 변경함

--- client/src/hoc/Auth.jsx
+++ client/src/hoc/Auth.jsx
@@ -3,18 +3,19 @@ import useStore from '../hook/use-store/useStore';
 import { observer } from 'mobx-react';
 import { useParams } from 'react-router-dom';
 
-export default (SpecificComponent: any): any => {
-  const AuthenticationCheck = (props: any) => {
+export default (SpecificComponent) => {
+  const AuthenticationCheck = (props) => {
     const { userStore } = useStore().rootStore;
-    const params = useParams<any>();
+    const params = useParams();
+    const accountbookId = params.id;
 
     useEffect(() => {
       // 로그인 검증
       userStore.checkAuth();
 
       // 선택한 가계부에대한 authority 검증
-      if (params.id) {
-        userStore.checkAuthority(params.id);
+      if (accountbookId) {
+        userStore.checkAuthority(accountbookId);
       }
     }, []);
     if (userStore.userId === null || userStore.isAdmin === null) {
