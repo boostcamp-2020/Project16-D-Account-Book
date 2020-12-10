e3b9bd2d58a5dec966d56d9f371ea61da1f2e313
mu1616 <mu1616@naver.com>
2020-12-10 18:26:16 +0900

    category 라우터에 권한검사 미들웨어 적용

--- server/src/api/routes/category.js
+++ server/src/api/routes/category.js
@@ -1,20 +1,16 @@
 const Router = require('koa-router');
 const categoryController = require('@controllers/category');
-const verificationOfAuth = require('@middlewares/jwt-authentication');
+const { isAdmin, isAccountbookUser } = require('@middlewares/accountbook-auth');
 
 const router = new Router();
 
-router.get('/income', verificationOfAuth, categoryController.getIncomeCategories);
-router.get('/expenditure', verificationOfAuth, categoryController.getExpenditureCategories);
-router.post('/income', verificationOfAuth, categoryController.createIncomeCategory);
-router.post('/expenditure', verificationOfAuth, categoryController.createExpenditureCategory);
-router.patch('/income/:income_category_id', verificationOfAuth, categoryController.updateIncomeCategory);
-router.patch('/expenditure/:expenditure_category_id', verificationOfAuth, categoryController.updateExpenditureCategory);
-router.delete('/income/:income_category_id', verificationOfAuth, categoryController.deleteIncomeCategory);
-router.delete(
-  '/expenditure/:expenditure_category_id',
-  verificationOfAuth,
-  categoryController.deleteExpenditureCategory,
-);
+router.get('/income', isAccountbookUser, categoryController.getIncomeCategories);
+router.get('/expenditure', isAccountbookUser, categoryController.getExpenditureCategories);
+router.post('/income', isAdmin, categoryController.createIncomeCategory);
+router.post('/expenditure', isAdmin, categoryController.createExpenditureCategory);
+router.patch('/income/:income_category_id', isAdmin, categoryController.updateIncomeCategory);
+router.patch('/expenditure/:expenditure_category_id', isAdmin, categoryController.updateExpenditureCategory);
+router.delete('/income/:income_category_id', isAdmin, categoryController.deleteIncomeCategory);
+router.delete('/expenditure/:expenditure_category_id', isAdmin, categoryController.deleteExpenditureCategory);
 
 module.exports = router;
