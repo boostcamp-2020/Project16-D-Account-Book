846046a3d5b285c38a303109b97ed55e08742797
bbbyung2 <bhko0524@naver.com>
2020-12-10 16:15:32 +0900

    chore: 사용하지 않는 코드 제거

--- client/src/components/common/modals/form-modal-account/FormModalUpdateAccount.tsx
+++ client/src/components/common/modals/form-modal-account/FormModalUpdateAccount.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import useStore from '../../../../hook/use-store/useStore';
 import FormModalWrapper from '../form-modal-template/FormModalWrapper';
 import FormModalItem from '../form-modal-template/FormModalItemWrapper';
