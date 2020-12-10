80d6c697fb9fe3847b00a3b6160111a51475fa8c
mu1616 <mu1616@naver.com>
2020-12-10 15:25:31 +0900

    feat: auth hoc 에서 가계부 권한 검증 로직 구현
    - params.id가 존재할 경우 해당 가계부에 대한 authority를 검증하도록 구현

--- client/src/hoc/Auth.tsx
+++ client/src/hoc/Auth.tsx
@@ -1,12 +1,20 @@
 import React, { useEffect } from 'react';
 import useStore from '../hook/use-store/useStore';
 import { observer } from 'mobx-react';
+import { useParams } from 'react-router-dom';
 
 export default (SpecificComponent: any): any => {
   const AuthenticationCheck = (props: any) => {
     const { userStore } = useStore().rootStore;
+    const params = useParams<any>();
+
     useEffect(() => {
+      // 로그인 검증
       userStore.checkAuth();
+      // 선택한 가계부에대한 authority 검증
+      if (params.id) {
+        userStore.checkAuthority(params.id);
+      }
     }, []);
     if (userStore.userId === null) {
       return <></>;
