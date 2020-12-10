f9fb618e6232a482eb4b2d61361758399a7e5f6a
mu1616 <mu1616@naver.com>
2020-12-10 09:33:56 +0900

    feat: sms 설명을 상수로 생성

--- client/src/constants/formModal.ts
+++ client/src/constants/formModal.ts
@@ -6,6 +6,7 @@ export const AccountLabelName = '결제수단 이름';
 export const CategoryLabelName = '카테고리 이름';
 export const UpdateAccountModalName = '결제수단 편집';
 export const UpdateCategorytModalName = '카테고리 편집';
+export const SMS_DESCRIPTION = '문자 메시지를 입력하면 해당 문자의 내용을 거래내역으로 자동 변환해주는 기능입니다.';
 
 const formModal = {
   AccountPlaceholder,
@@ -16,6 +17,7 @@ const formModal = {
   CategoryLabelName,
   UpdateAccountModalName,
   UpdateCategorytModalName,
+  SMS_DESCRIPTION,
 };
 
 export default formModal;
