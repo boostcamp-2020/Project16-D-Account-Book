a3afceb5ebf561a3ee97db02dc58a0f78054aa68
lacomaco <lacomaco122@gmail.com>
2020-12-10 18:08:20 +0900

    feat : InputRadio Stories 교체

--- client/src/components/common/inputs/Inputs.stories.tsx
+++ client/src/components/common/inputs/Inputs.stories.tsx
@@ -9,6 +9,7 @@ import MultiInputDropdownWithCheckBox from './multi-input-dropdown/MultiInputDro
 import ModalClassify from './modal-classify/ModalClassify';
 import InputDateTime from './input-datetime/InputDateTime';
 import InputRadio from './input-radio/InputRadio';
+import { startDateChecker } from '../../../types/inputRadio';
 
 const SmallDiv = styled.div`
   width: 300px;
@@ -81,5 +82,6 @@ export const InputDateTimeDefault: React.FC = () => {
 };
 
 export const InputRadioDefault: React.FC = () => {
-  return <InputRadio />;
+  const { name, left, right } = startDateChecker;
+  return <InputRadio name={name} left={left} right={right} />;
 };
