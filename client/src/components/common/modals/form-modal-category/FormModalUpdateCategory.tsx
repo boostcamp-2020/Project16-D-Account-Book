1a37e0de054a202c81ce7ecf2d928d7548560e6d
bbbyung2 <bhko0524@naver.com>
2020-12-11 04:22:37 +0900

    feat: 수입/지출 카테고리 수정 시 실시간 중복 검사 기능 구현

--- client/src/components/common/modals/form-modal-category/FormModalUpdateCategory.tsx
+++ client/src/components/common/modals/form-modal-category/FormModalUpdateCategory.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useStore from '../../../../hook/use-store/useStore';
 import FormModalWrapper from '../form-modal-template/FormModalWrapper';
 import FormModalItem from '../form-modal-template/FormModalItemWrapper';
@@ -12,12 +12,19 @@ import formModal from '../../../../constants/formModal';
 import { Category } from '../../../../types/category';
 import { convertToCategory } from '../formUtils';
 import useGetParam from '../../../../hook/use-get-param/useGetParam';
+import CheckSuccess from '../../check/check-success/CheckSuccess';
+import CheckFail from '../../check/check-fail/CheckFail';
+import CheckSuccessText from '../../check/check-text/CheckSuccessText';
+import CheckFailText from '../../check/check-text/CheckFailText';
+import CheckNoActionText from '../../check/check-text/CheckNoActionText';
+import CheckNoAction from '../../check/check-no-action/CheckNoAction';
 
 const FormModalUpdateCategory: React.FC = () => {
   const { rootStore } = useStore();
   const id = useGetParam();
   const updateCategoryFormStore = rootStore.modalStore.updateCategoryFormStore;
   const { show } = updateCategoryFormStore;
+  const { check, noChange } = rootStore.modalStore.updateCategoryFormStore;
 
   const [name, setName] = useState<string>(
     updateCategoryFormStore.incomeFlag
@@ -33,8 +40,39 @@ const FormModalUpdateCategory: React.FC = () => {
   const incomeCategoryId = updateCategoryFormStore.incomeCategory?.id;
   const expenditureCategoryId = updateCategoryFormStore.expenditureCategory?.id;
 
+  useEffect(() => {
+    updateCategoryFormStore.incomeFlag
+      ? updateCategoryFormStore.loadOriginalIncomeCategory((updateCategoryFormStore.incomeCategory as Category).name)
+      : updateCategoryFormStore.loadOriginalExpenditureCategory(
+          (updateCategoryFormStore.expenditureCategory as Category).name,
+        );
+  }, []);
+
   const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
+    if (updateCategoryFormStore.incomeFlag) {
+      if (rootStore.categoryStore.incomeCategoryNames.includes(e.target.value)) {
+        rootStore.modalStore.updateCategoryFormStore.setCheckFalse();
+      } else {
+        rootStore.modalStore.updateCategoryFormStore.setCheckTrue();
+      }
+      if (updateCategoryFormStore.originalIncomeCategoryName === e.target.value) {
+        rootStore.modalStore.updateCategoryFormStore.setNoChangeTrue();
+      } else {
+        rootStore.modalStore.updateCategoryFormStore.setNoChangeFalse();
+      }
+    } else {
+      if (rootStore.categoryStore.expenditureCategoryNames.includes(e.target.value)) {
+        rootStore.modalStore.updateCategoryFormStore.setCheckFalse();
+      } else {
+        rootStore.modalStore.updateCategoryFormStore.setCheckTrue();
+      }
+      if (updateCategoryFormStore.originalExpenditureCategoryName === e.target.value) {
+        rootStore.modalStore.updateCategoryFormStore.setNoChangeTrue();
+      } else {
+        rootStore.modalStore.updateCategoryFormStore.setNoChangeFalse();
+      }
+    }
   };
 
   const onChange = (color: { hex: string }): void => {
@@ -43,6 +81,7 @@ const FormModalUpdateCategory: React.FC = () => {
 
   const modalToggle = (): void => {
     updateCategoryFormStore.toggleShow();
+    updateCategoryFormStore.setNoChangeTrue();
   };
 
   const deleteIncomeCategory = (): void => {
@@ -88,25 +127,75 @@ const FormModalUpdateCategory: React.FC = () => {
   return (
     <ModalBackground show={show} closeModal={modalToggle}>
       <FormModalWrapper>
-        <FormModalHeader
-          modalName={
-            updateCategoryFormStore.incomeFlag
-              ? formModal.UPDATE_INCOME_CATEGORY_MODAL_NAME
-              : formModal.UPDATE_EXPENDITURE_CATEGORY_MODAL_NAME
-          }
-          blueName={'완료'}
-          redName={'삭제'}
-          closeModal={modalToggle}
-          clickBlue={updateCategoryFormStore.incomeFlag ? updateIncomeCategory : updateExpenditureCategory}
-          clickRed={updateCategoryFormStore.incomeFlag ? deleteIncomeCategory : deleteExpenditureCategory}
-        />
+        {check ? (
+          name && !noChange ? (
+            <FormModalHeader
+              modalName={
+                updateCategoryFormStore.incomeFlag
+                  ? formModal.UPDATE_INCOME_CATEGORY_MODAL_NAME
+                  : formModal.UPDATE_EXPENDITURE_CATEGORY_MODAL_NAME
+              }
+              blueName={'완료'}
+              redName={'삭제'}
+              closeModal={modalToggle}
+              clickBlue={updateCategoryFormStore.incomeFlag ? updateIncomeCategory : updateExpenditureCategory}
+              clickRed={updateCategoryFormStore.incomeFlag ? deleteIncomeCategory : deleteExpenditureCategory}
+            />
+          ) : (
+            <FormModalHeader
+              modalName={
+                updateCategoryFormStore.incomeFlag
+                  ? formModal.UPDATE_INCOME_CATEGORY_MODAL_NAME
+                  : formModal.UPDATE_EXPENDITURE_CATEGORY_MODAL_NAME
+              }
+              redName={'삭제'}
+              closeModal={modalToggle}
+              clickRed={updateCategoryFormStore.incomeFlag ? deleteIncomeCategory : deleteExpenditureCategory}
+              disabledName={'완료'}
+            />
+          )
+        ) : (
+          <FormModalHeader
+            modalName={
+              updateCategoryFormStore.incomeFlag
+                ? formModal.UPDATE_INCOME_CATEGORY_MODAL_NAME
+                : formModal.UPDATE_EXPENDITURE_CATEGORY_MODAL_NAME
+            }
+            redName={'삭제'}
+            closeModal={modalToggle}
+            clickRed={updateCategoryFormStore.incomeFlag ? deleteIncomeCategory : deleteExpenditureCategory}
+            disabledName={'완료'}
+          />
+        )}
         <FormModalItem>
           <CategoryPreview name={name} color={inputColor} onChange={onChange} />
         </FormModalItem>
         <FormModalItem>
           <FormModalLabel>{formModal.CATEGORY_LABEL_NAME}</FormModalLabel>
           <InputText maxLength={8} placeholder={formModal.CATEGORY_PLACEHOLDER} value={name} onChange={onChangeName} />
+          {check ? (
+            name && !noChange ? (
+              <CheckSuccess />
+            ) : (
+              <CheckNoAction />
+            )
+          ) : !noChange ? (
+            <CheckFail />
+          ) : (
+            <CheckNoAction />
+          )}
         </FormModalItem>
+        {check ? (
+          name && !noChange ? (
+            <CheckSuccessText />
+          ) : (
+            <CheckNoActionText />
+          )
+        ) : !noChange ? (
+          <CheckFailText />
+        ) : (
+          <CheckNoActionText />
+        )}
       </FormModalWrapper>
     </ModalBackground>
   );
