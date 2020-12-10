6b67b373c6841653a464dd47e7694ea86c9f45f4
Mong-Gu <yhw8258@naver.com>
2020-12-11 04:34:13 +0900

    refactor: 스토리북에 페이지와 같은 view wrapper 적용

--- client/src/components/accountbook-selection-page/accountbook-card/AccountbookCard.stories.tsx
+++ client/src/components/accountbook-selection-page/accountbook-card/AccountbookCard.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import AccountbookCard from './AccountbookCard';
 
 export default {
@@ -6,29 +7,53 @@ export default {
   component: AccountbookCard,
 };
 
+const ViewWrapper = styled.div`
+  width: 40%;
+  padding-top: 5%;
+  margin: 0 auto;
+`;
+
 export const WhiteTextAccountbookCard = (): JSX.Element => {
-  return <AccountbookCard id={1} name={'가계부1'} color={'#000000'} description={'가계부1 설명'} />;
+  return (
+    <ViewWrapper>
+      <AccountbookCard id={1} title={'가계부1'} color={'#000000'} description={'가계부1 설명'} />
+    </ViewWrapper>
+  );
 };
 
 export const BlackTextAccountbookCard = (): JSX.Element => {
-  return <AccountbookCard id={2} name={'가계부2'} color={'#E0F8EC'} description={'가계부2 설명'} />;
+  return (
+    <ViewWrapper>
+      <AccountbookCard id={2} title={'가계부2'} color={'#E0F8EC'} description={'가계부2 설명'} />
+    </ViewWrapper>
+  );
 };
 
 export const BlueBackgroundAccountbookCard = (): JSX.Element => {
-  return <AccountbookCard id={3} name={'가계부2'} color={'#1EA7FD'} description={'가계부3 설명'} />;
+  return (
+    <ViewWrapper>
+      <AccountbookCard id={3} title={'가계부3'} color={'#1EA7FD'} description={'가계부3 설명'} />
+    </ViewWrapper>
+  );
 };
 
 export const RedBackgroundAccountbookCard = (): JSX.Element => {
-  return <AccountbookCard id={4} name={'가계부2'} color={'#F35454'} description={'가계부4 설명'} />;
+  return (
+    <ViewWrapper>
+      <AccountbookCard id={4} title={'가계부4'} color={'#F35454'} description={'가계부4 설명'} />
+    </ViewWrapper>
+  );
 };
 
 export const LongTextAccountbookCard = (): JSX.Element => {
   return (
-    <AccountbookCard
-      id={5}
-      name={'가계부가계부가계부가계부가계부'}
-      color={'#E0F8EC'}
-      description={'30자맞춘테스트설명30자맞춘테스트설명30자맞춘테스트설명'}
-    />
+    <ViewWrapper>
+      <AccountbookCard
+        id={5}
+        title={'가계부가계부가계부가계부가계부'}
+        color={'#E0F8EC'}
+        description={'30자맞춘테스트설명30자맞춘테스트설명30자맞춘테스트설명'}
+      />
+    </ViewWrapper>
   );
 };
