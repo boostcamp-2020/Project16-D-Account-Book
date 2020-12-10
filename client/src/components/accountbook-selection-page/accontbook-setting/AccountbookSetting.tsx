886264008d52f2de1a0c951e5349b452f3ea8a6d
Mong-Gu <yhw8258@naver.com>
2020-12-11 04:01:33 +0900

    feat: div 호버 시 svg 선택되었다는 표현 방식 설정
    논의가 필요

--- client/src/components/accountbook-selection-page/accontbook-setting/AccountbookSetting.tsx
+++ client/src/components/accountbook-selection-page/accontbook-setting/AccountbookSetting.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 import { getTextColor } from '../../../utils/color';
+import { BLUE, NAVER_GREEN } from '../../../constants/color';
 
-const Wrapper = styled.div<{ itemColor: string }>`
+const Wrapper = styled.div<{ itemColor: string; bgColor: string }>`
   width: 1rem;
   height: 1rem;
   box-sizing: border-box;
   .setting {
     fill: ${({ itemColor }) => itemColor};
   }
+  /* &:hover .setting {
+    fill: ${({ bgColor }) => {
+    return bgColor === BLUE ? NAVER_GREEN : BLUE;
+  }};
+  } */
+  &:hover .setting {
+    fill: ${BLUE};
+    stroke: ${({ bgColor, itemColor }) => {
+      return bgColor === BLUE ? itemColor : null;
+    }};
+    stroke-width: ${({ bgColor }) => {
+      return bgColor === BLUE ? '1px' : null;
+    }};
+  }
 `;
 
 interface SettingProps {
-  accountbookId: number;
+  id: number;
   bgColor: string;
 }
 
-const AccountbookSetting = ({ accountbookId, bgColor }: SettingProps): JSX.Element => {
+const AccountbookSetting = ({ id, bgColor }: SettingProps): JSX.Element => {
   const itemColor = getTextColor(bgColor);
   return (
-    <Wrapper itemColor={itemColor}>
+    <Wrapper itemColor={itemColor} bgColor={bgColor}>
       <svg viewBox="0 0 24 24" className="setting">
         <path d="m22.683 9.394-1.88-.239c-.155-.477-.346-.937-.569-1.374l1.161-1.495c.47-.605.415-1.459-.122-1.979l-1.575-1.575c-.525-.542-1.379-.596-1.985-.127l-1.493 1.161c-.437-.223-.897-.414-1.375-.569l-.239-1.877c-.09-.753-.729-1.32-1.486-1.32h-2.24c-.757 0-1.396.567-1.486 1.317l-.239 1.88c-.478.155-.938.345-1.375.569l-1.494-1.161c-.604-.469-1.458-.415-1.979.122l-1.575 1.574c-.542.526-.597 1.38-.127 1.986l1.161 1.494c-.224.437-.414.897-.569 1.374l-1.877.239c-.753.09-1.32.729-1.32 1.486v2.24c0 .757.567 1.396 1.317 1.486l1.88.239c.155.477.346.937.569 1.374l-1.161 1.495c-.47.605-.415 1.459.122 1.979l1.575 1.575c.526.541 1.379.595 1.985.126l1.494-1.161c.437.224.897.415 1.374.569l.239 1.876c.09.755.729 1.322 1.486 1.322h2.24c.757 0 1.396-.567 1.486-1.317l.239-1.88c.477-.155.937-.346 1.374-.569l1.495 1.161c.605.47 1.459.415 1.979-.122l1.575-1.575c.542-.526.597-1.379.127-1.985l-1.161-1.494c.224-.437.415-.897.569-1.374l1.876-.239c.753-.09 1.32-.729 1.32-1.486v-2.24c.001-.757-.566-1.396-1.316-1.486zm-10.683 7.606c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z" />
       </svg>
--- client/src/components/accountbook-selection-page/accountbook-elimination/AccountbookElimination.tsx
+++ client/src/components/accountbook-selection-page/accountbook-elimination/AccountbookElimination.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 import { getTextColor } from '../../../utils/color';
+import { RED, NAVER_GREEN } from '../../../constants/color';
 
-const Wrapper = styled.div<{ itemColor: string }>`
+const Wrapper = styled.div<{ itemColor: string; bgColor: string }>`
   width: 1rem;
   height: 1rem;
   box-sizing: border-box;
   .elimination {
     fill: ${({ itemColor }) => itemColor};
   }
+  /* &:hover .elimination {
+    fill: ${({ bgColor }) => {
+    return bgColor === RED ? NAVER_GREEN : RED;
+  }};
+  } */
+  &:hover .elimination {
+    fill: ${RED};
+    stroke: ${({ bgColor, itemColor }) => {
+      return bgColor === RED ? itemColor : null;
+    }};
+    stroke-width: ${({ bgColor }) => {
+      return bgColor === RED ? '10px' : null;
+    }};
+  }
 `;
 
 interface SettingProps {
-  accountbookId: number;
+  id: number;
   bgColor: string;
 }
 
-const AccountbookElimination = ({ accountbookId, bgColor }: SettingProps): JSX.Element => {
+const AccountbookElimination = ({ id, bgColor }: SettingProps): JSX.Element => {
   const itemColor = getTextColor(bgColor);
   return (
-    <Wrapper itemColor={itemColor}>
+    <Wrapper itemColor={itemColor} bgColor={bgColor}>
       <svg viewBox="0 0 329.26933 329" className="elimination">
         <path d="m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0" />
       </svg>
