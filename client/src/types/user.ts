64765e9acf60327b9f4f00aa7b9d907f7150980e
lacomaco <lacomaco122@gmail.com>
2020-12-12 01:52:26 +0900

    feat : API 타입 User 수정
    - isAdmin은 더이상 사용하지 않는 속성이 되었고
    - 대신 userAccountbooks 타입에 유저의 가계부 권한을 저장할 수 있도록
      타입을 수정하였습니다.

--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -3,6 +3,12 @@ export interface User {
   provider: string;
   nickname: string;
   profileUrl: string;
+  userAccountbooks: UserAuthorType[];
+}
+
+export interface UserAuthorType {
+  authority: boolean;
+  accountbookId: number;
 }
 
 export default User;
