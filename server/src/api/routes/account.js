2038876ea51089a273c1b80790fb3cdf575bbeb9
mu1616 <mu1616@naver.com>
2020-12-11 22:09:25 +0900

    refactor: 백엔드 결제수단 api 권한 수정
    - 일반유저도 수정/삭제/생성이 가능하도록 수정

--- server/src/api/routes/account.js
+++ server/src/api/routes/account.js
@@ -1,12 +1,12 @@
 const Router = require('koa-router');
 const accountController = require('@controllers/account');
-const { isAdmin, isAccountbookUser } = require('@middlewares/accountbookAuth');
+const { isAccountbookUser } = require('@middlewares/accountbookAuth');
 
 const router = new Router();
 
 router.get('/', isAccountbookUser, accountController.getAccounts);
-router.post('/', isAdmin, accountController.createAccount);
-router.patch('/:account_id', isAdmin, accountController.updateAccount);
-router.delete('/:account_id', isAdmin, accountController.deleteAccount);
+router.post('/', isAccountbookUser, accountController.createAccount);
+router.patch('/:account_id', isAccountbookUser, accountController.updateAccount);
+router.delete('/:account_id', isAccountbookUser, accountController.deleteAccount);
 
 module.exports = router;
