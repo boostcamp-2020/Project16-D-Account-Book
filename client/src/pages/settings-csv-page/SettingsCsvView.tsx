127e3f85947efce835865b318721203aeaa50843
bbbyung2 <bhko0524@naver.com>
2020-12-12 02:02:10 +0900

    refactor: SettingsCsvPage에서 분리된 SettingsCsvView 컴포넌트 추가

---
+++ client/src/pages/settings-csv-page/SettingsCsvView.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import styled from 'styled-components';
+import ExportButton from '../../components/export-button/ExportButton';
+import ImportButton from '../../components/import-button/ImportButton';
+
+interface Props {
+  accountbookId: number;
+}
+
+export const SettingsCsvViewWrapper = styled.div`
+  position: absolute;
+  top: 0%;
+  left: 25%;
+  padding: 50px 50px;
+`;
+
+const SettingsItemWrapper = styled.div`
+  width: 70vw;
+  margin-top: 50px;
+  margin-bottom: 25vh;
+`;
+
+const Label = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 4vh;
+  display: flex;
+`;
+
+const Content = styled.p`
+  margin: 0 0 0.5vh 0;
+  font-size: 1.1rem;
+`;
+
+const SettingsCsvView: React.FC<Props> = ({ accountbookId }: Props) => {
+  return (
+    <SettingsCsvViewWrapper>
+      <SettingsItemWrapper>
+        <Label>
+          데이터 내보내기
+          <ExportButton />
+        </Label>
+        <Content>현재 선택한 가계부에 기록되어 있는 거래 내역을 CSV 파일로 생성하여 다운받으실 수 있습니다.</Content>
+      </SettingsItemWrapper>
+      <SettingsItemWrapper>
+        <Label>
+          데이터 불러오기
+          <ImportButton />
+        </Label>
+        <Content>선택한 CSV 파일 내용을 현재 가계부 내용에 추가하실 수 있습니다.</Content>
+        <Content>단, CSV 파일은 다음의 형식을 지켜야 합니다.</Content>
+      </SettingsItemWrapper>
+    </SettingsCsvViewWrapper>
+  );
+};
+
+export default observer(SettingsCsvView);
