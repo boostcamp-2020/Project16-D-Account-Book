c59904a10633bab953c56eff0c0cf3ab9fd98c81
mu1616 <mu1616@naver.com>
2020-12-13 03:43:27 +0900

    style: 필요없는 코드 삭제

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,14 +1,5 @@
-import io, { Socket } from 'socket.io-client';
+import io from 'socket.io-client';
 const socket = io.connect(process.env.REACT_APP_BASE_URL as string);
-
-// export const connectSocket = (accountbookId: number): typeof Socket => {
-//   socket = io.connect(process.env.REACT_APP_BASE_URL as string);
-//   return socket;
-// };
-
-// export const getSocket = (): typeof Socket => {
-//   return socket;
-// };
 
 export const event = {
   UPDATE_TRANSACTIONS: 'update transactions',
