29e303131ee0530836a31d23f66a88aa5782c894
lacomaco <lacomaco122@gmail.com>
2020-12-12 23:21:45 +0900

    feat : LoginGuard 수정
    - 로그인 하지 않은 유저가 접근할 경우, 빈 화면을 보여준후 리다이렉트
      시키는 HOC 함수를 생성하였습니다.

--- client/src/router/LoginGuard.tsx
+++ client/src/router/LoginGuard.tsx
@@ -1,27 +1,19 @@
 import { observer } from 'mobx-react';
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-import useGetParam from '../hook/use-get-param/useGetParam';
 import useStore from '../hook/use-store/useStore';
 
-const LoginGuard = (HOC: React.ComponentType<any>): React.FC => {
-  const LoginGuardChecker = (props: any) => {
+const LoginGuardHOC = (HOC: React.ComponentType<any>): React.FC => {
+  const LoginGuard = (props: any) => {
     const { userStore } = useStore().rootStore;
-    const accountbookId = useGetParam();
-
-    const flag = userStore.isUserAdmin(accountbookId);
 
     if (userStore.accountAuthorList === null) {
       return <></>;
     }
 
-    if (flag) {
-      return <HOC {...props} />;
-    }
-
-    return <Redirect to="/" />;
+    return <HOC {...props} />;
   };
-  return observer(LoginGuardChecker);
+
+  return observer(LoginGuard);
 };
 
-export default LoginGuard;
+export default LoginGuardHOC;
