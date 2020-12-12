269da88f129621bd5dc14667e963a40da03eea1c
lacomaco <lacomaco122@gmail.com>
2020-12-12 23:22:52 +0900

    feat : 유저 권한 가드 HOC 생성
    - 권한 인증이 필요한 페이지 접근시 차단하는 HOC 컴포넌트를
      생성하였습니다.

---
+++ client/src/router/UserAuthorityGuard.tsx
@@ -0,0 +1,27 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import useGetParam from '../hook/use-get-param/useGetParam';
+import useStore from '../hook/use-store/useStore';
+
+const UserAuthorityGuardHOC = (HOC: React.ComponentType<any>): React.FC => {
+  const UserAuthorityGuard = (props: any) => {
+    const { userStore } = useStore().rootStore;
+    const accountbookId = useGetParam();
+
+    const flag = userStore.isUserAdmin(accountbookId);
+
+    if (userStore.accountAuthorList === null) {
+      return <></>;
+    }
+
+    if (flag) {
+      return <HOC {...props} />;
+    }
+
+    return <Redirect to="/" />;
+  };
+  return observer(UserAuthorityGuard);
+};
+
+export default UserAuthorityGuardHOC;
