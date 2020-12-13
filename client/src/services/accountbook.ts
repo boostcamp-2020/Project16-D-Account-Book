bd2118b3e26c38beb93040514f0b0708964f5146
bbbyung2 <bhko0524@naver.com>
2020-12-14 02:18:45 +0900

    feat: 가계부 수정 API 요청하는 service 함수 구현

--- client/src/services/accountbook.ts
+++ client/src/services/accountbook.ts
@@ -3,6 +3,7 @@ import Accountbook from '../types/accountbook';
 
 const accountbookAPIAddress = {
   getAccountbooks: '/api/accountbooks',
+  updateAccountbook: '/api/accountbooks',
   deleteAccountbook: '/api/accountbooks',
 };
 
@@ -10,6 +11,14 @@ export default {
   getAccountbooks: async (): Promise<Accountbook[]> => {
     const response = await instance.get(`${accountbookAPIAddress.getAccountbooks}`);
     return response.data;
+  },
+  updateAccountbook: async (accountbookId: number): Promise<void> => {
+    try {
+      const response = await instance.patch(`${accountbookAPIAddress.updateAccountbook}/${accountbookId}`);
+      return response.data;
+    } catch {
+      throw new Error('수정 실패');
+    }
   },
   deleteAccountbook: async (accountbookId: number): Promise<void> => {
     await instance.delete(`${accountbookAPIAddress.deleteAccountbook}/${accountbookId}`);
