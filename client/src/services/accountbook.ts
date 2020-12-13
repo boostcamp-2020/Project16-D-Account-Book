ea5e279372c3cff8a31f1ac20797613a1eec66b0
Mong-Gu <yhw8258@naver.com>
2020-12-13 23:18:22 +0900

    feat: 가계부 불러오기 api client와 연동

--- client/src/services/accountbook.ts
+++ client/src/services/accountbook.ts
@@ -1,10 +1,16 @@
 import instance from '../api/axios';
+import Accountbook from '../types/accountbook';
 
 const accountbookAPIAddress = {
+  getAccountbooks: '/api/accountbooks',
   deleteAccountbook: '/api/accountbooks',
 };
 
 export default {
+  getAccountbooks: async (): Promise<Accountbook[]> => {
+    const response = await instance.get(`${accountbookAPIAddress.getAccountbooks}`);
+    return response.data;
+  },
   deleteAccountbook: async (accountbookId: number): Promise<void> => {
     await instance.delete(`${accountbookAPIAddress.deleteAccountbook}/${accountbookId}`);
   },
