d3c1d9b0ec9a8741be012027be9c80d30197fc1f
mu1616 <mu1616@naver.com>
2020-12-14 15:56:00 +0900

    feat: 가계부 생성 모달 preview 부분 구현
    - 가계부 preview 부분을 구현하였습니다.

--- client/src/components/common/modals/form-modal-accountbook/FormModalCreateAccountbook.tsx
+++ client/src/components/common/modals/form-modal-accountbook/FormModalCreateAccountbook.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import ModalBackground from '../modal-background/ModalBackground';
 import { MODAL_WHITE } from '../../../../constants/color';
 import useStore from '../../../../hook/use-store/useStore';
 import FormModalHeader from '../form-modal-header/FormModalHeader';
+import Preview from '../../preview/Preview';
 
 const FormModalWrapper = styled.div`
   background-color: ${MODAL_WHITE};
@@ -12,12 +13,24 @@ const FormModalWrapper = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
-  height: 360px;
   transform: translate(-50%, -50%);
+`;
+
+const PreviewWrapper = styled.div`
+  margin-top: 1rem;
+  width: 80%;
 `;
 
 const FormModalCreateAccountbook = (): JSX.Element => {
   const { createAccountbookFormStore } = useStore().rootStore.modalStore;
+  const [inputColor, setInputColor] = useState<string>('black');
+  const [title, setTitle] = useState<string>('가계부');
+  const [description, setDescription] = useState<string>('가계부에 대한 설명을 입력하세요.');
+
+  const onChangeColor = (color: { hex: string }): void => {
+    setInputColor(color.hex);
+  };
+
   return (
     <ModalBackground show={true} closeModal={() => createAccountbookFormStore.setShow(false)}>
       <FormModalWrapper>
@@ -27,6 +40,9 @@ const FormModalCreateAccountbook = (): JSX.Element => {
           clickBlue={undefined}
           closeModal={() => createAccountbookFormStore.setShow(false)}
         />
+        <PreviewWrapper>
+          <Preview title={title} description={description} color={inputColor} onChange={onChangeColor} />
+        </PreviewWrapper>
       </FormModalWrapper>
     </ModalBackground>
   );
