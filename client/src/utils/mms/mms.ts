eca54978ccd6564d65546e60d2f900fa8ccea18c
lacomaco <lacomaco122@gmail.com>
2020-12-14 19:10:54 +0900

    feat : MMS util 함수 생성
    - getDateString : MMSType의 date와 time 속성을 조합하여 ISOString 형태의
      문자열을 반환하는 함수입니다.
    
    - getMonthDay : date 문자열을 받아, 달과 일을 분리하는 함수입니다.
    - getHourMinute : time 문자열을 받아 시간과 분을 분리하는 함수입니다.
    - findAccountsByName : 결제수단 배열에서 같은 이름을 가지고 있는
      결제수단을 찾아서 반환하는 함수입니다.

---
+++ client/src/utils/mms/mms.ts
@@ -0,0 +1,26 @@
+import Account from '../../types/account';
+
+export const getDateString = (date: string, time: string): string => {
+  const [month, day] = getMonthDay(date);
+  const [hour, minute] = getHourMinute(time);
+  const currentTime = new Date();
+  currentTime.setMonth(month - 1);
+  currentTime.setDate(day);
+  currentTime.setHours(hour);
+  currentTime.setMinutes(minute);
+  return currentTime.toISOString().replace(/Z/g, '');
+};
+
+const getMonthDay = (date: string): [month: number, day: number] => {
+  const stringMonthDay = date.split('/');
+  return [parseInt(stringMonthDay[0]), parseInt(stringMonthDay[1])];
+};
+
+const getHourMinute = (time: string): [hour: number, minute: number] => {
+  const stringHourMinute = time.split(':');
+  return [parseInt(stringHourMinute[0]), parseInt(stringHourMinute[1])];
+};
+
+export const findAccountByName = (name: string, categories: Account[]): Account | undefined => {
+  return categories.find((category) => category.name.startsWith(name));
+};
