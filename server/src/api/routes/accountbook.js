9826671e257cb79edcbd3de64ad04e6876c04068
Mong-Gu <yhw8258@naver.com>
2020-12-14 16:36:18 +0900

    feat: 가계부 생성 라우터 연결

--- server/src/api/routes/accountbook.js
+++ server/src/api/routes/accountbook.js
@@ -6,6 +6,7 @@ const { isAccountbookUser } = require('@middlewares/accountbookAuth');
 const router = new Router();
 
 router.get('/', verificationOfAuth, accountbookController.getAccountbooksByUserId);
+router.post('/', isAccountbookUser, accountbookController.createAccountbook);
 router.delete('/:accountbook_id', isAccountbookUser, accountbookController.deleteAccountbook);
 
 module.exports = router;
