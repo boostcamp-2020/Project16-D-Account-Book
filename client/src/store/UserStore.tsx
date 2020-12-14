23d3ee2dab2ba44a4d1c9bef6b83aac276e6a4cd
lacomaco <lacomaco122@gmail.com>
2020-12-14 10:52:29 +0900

    fix : 로그아웃시 회원 데이터를 전부 삭제

--- client/src/store/UserStore.tsx
+++ client/src/store/UserStore.tsx
@@ -48,7 +48,7 @@ export default class UserStore {
     this.provider = '';
     this.nickname = '';
     this.profileUrl = '';
-    this.accountAuthorList = [];
+    this.accountAuthorList = null;
   };
 
   getAuthority = async (): Promise<void> => {
