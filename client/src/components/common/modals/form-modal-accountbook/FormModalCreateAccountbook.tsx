e6ef197e655284d17c6864ff56026eadca9af940
mu1616 <mu1616@naver.com>
2020-12-14 16:15:10 +0900

    feat: 가계부 생성 모달창 UI 완성
    - 가계부 생성 모달창의 UI를 완성시켰습니다.
    - 생성 버튼을 눌렀을 때 실행할 이벤트를 추가로 구현해야합니다.

--- client/src/components/common/modals/form-modal-accountbook/FormModalCreateAccountbook.tsx
+++ client/src/components/common/modals/form-modal-accountbook/FormModalCreateAccountbook.tsx
@@ -5,15 +5,17 @@ import { MODAL_WHITE } from '../../../../constants/color';
 import useStore from '../../../../hook/use-store/useStore';
 import FormModalHeader from '../form-modal-header/FormModalHeader';
 import Preview from '../../preview/Preview';
+import InputText from '../../inputs/input-text/InputText';
 
 const FormModalWrapper = styled.div`
   background-color: ${MODAL_WHITE};
-  width: 40%;
+  width: 50%;
   padding: 20px;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  height: 400px;
 `;
 
 const PreviewWrapper = styled.div`
@@ -21,14 +23,33 @@ const PreviewWrapper = styled.div`
   width: 80%;
 `;
 
+const Label = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+const ContentWrapper = styled.div`
+  margin-top: 1rem;
+  width: 80%;
+`;
+
 const FormModalCreateAccountbook = (): JSX.Element => {
   const { createAccountbookFormStore } = useStore().rootStore.modalStore;
   const [inputColor, setInputColor] = useState<string>('black');
-  const [title, setTitle] = useState<string>('가계부');
-  const [description, setDescription] = useState<string>('가계부에 대한 설명을 입력하세요.');
+  const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
 
   const onChangeColor = (color: { hex: string }): void => {
     setInputColor(color.hex);
+  };
+
+  const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const onChangeDescription = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(e.target.value);
   };
 
   return (
@@ -43,6 +64,24 @@ const FormModalCreateAccountbook = (): JSX.Element => {
         <PreviewWrapper>
           <Preview title={title} description={description} color={inputColor} onChange={onChangeColor} />
         </PreviewWrapper>
+        <ContentWrapper>
+          <Label>가계부 별칭</Label>
+          <InputText
+            maxLength={15}
+            placeholder={'최대 15자의 가계부 별칭을 작성해주세요.'}
+            value={title}
+            onChange={onChangeTitle}
+          />
+        </ContentWrapper>
+        <ContentWrapper>
+          <Label>가계부 설명</Label>
+          <InputText
+            maxLength={30}
+            placeholder={'가계부에 대한 설명을 기재해주세요.'}
+            value={description}
+            onChange={onChangeDescription}
+          />
+        </ContentWrapper>
       </FormModalWrapper>
     </ModalBackground>
   );
