5beb917837633ed1641001d875809028ae11d7fe
lacomaco <lacomaco122@gmail.com>
2020-12-14 19:13:38 +0900

    feat : MMSInput 컴포넌트 생성
    - 텍스트 박스의 값을 서버에 보내 받아온 정보를 바탕으로 InputForm을
      변환시키는 컴포넌트입니다.
    
    - transactionType이 거절이라면 아무것도 하지 alert 창을 띄웁니다.
    - 카드이름을 찾을 수 없으면 alert 창을 띄우고 이후 작업을 수행합니다.
    - date,amount,isDeposit 속성은 존재하면 폼을 변경합니다.

--- client/src/components/common/modals/form-modal-transaction/MMSInput.tsx
+++ client/src/components/common/modals/form-modal-transaction/MMSInput.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import color from '../../../../constants/color';
 import { InputListWrapper, InputWrapper } from './TransactionInputList';
-
+import { FormActionType, FormChangeAction } from '../../../../types/TransactionForm';
+import { getDateString, findAccountByName } from '../../../../utils/mms/mms';
+import transactionService from '../../../../services/transaction';
+import useStore from '../../../../hook/use-store/useStore';
 const SmallArea = styled.div`
   width: 70%;
   margin: 10px auto;
@@ -9,7 +13,7 @@ const SmallArea = styled.div`
 
 const TextBox = styled.textarea`
   width: 100%;
-  height: 200px;
+  height: 400px;
   margin: 10px auto;
   resize: none;
   border: 1px solid lightgray;
@@ -23,15 +27,79 @@ const ButtonLayout = styled.div`
   margin: 5px 0px;
   justify-content: space-between;
 `;
-const MMSInput: React.FC = () => {
+
+interface IButtons {
+  backgroundColor: string;
+}
+const Buttons = styled.button<IButtons>`
+  width: 40%;
+  padding: 10px 10px;
+  border-radius: 50px;
+  border: 0px;
+  color: white;
+  cursor: pointer;
+  background-color: ${(props) => props.backgroundColor};
+`;
+
+interface IMMSInput {
+  dispatch: React.Dispatch<FormChangeAction>;
+  MMSToggle: () => void;
+}
+
+const MMSInput: React.FC<IMMSInput> = ({ dispatch, MMSToggle }: IMMSInput) => {
+  const [mmsText, setMMSText] = useState('');
+  const { accountStore } = useStore().rootStore;
+  const textChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMMSText(e.target.value);
+  };
+  const clickConfirm = () => {
+    const mmsParsing = async () => {
+      //
+      const response = await transactionService.parsingMMS(mmsText);
+      if (response.transactionType === '거절') {
+        alert('처리할 수 없는 결제 내역입니다.');
+        return;
+      }
+      const selectedCategory = findAccountByName(response.cardname, accountStore.accounts);
+      if (selectedCategory === undefined) {
+        alert('카드 이름을 찾을 수 없습니다. 직접 입력해주세요!');
+      } else {
+        dispatch({
+          type: FormActionType.ACCOUNTS_CHANGE,
+          data: selectedCategory.id + '',
+        });
+      }
+
+      dispatch({
+        type: FormActionType.CLASSIFY_CHANGE,
+        data: response.isDeposit,
+      });
+
+      dispatch({
+        type: FormActionType.PRICE_CHANGE,
+        data: response.amount,
+      });
+
+      dispatch({
+        type: FormActionType.DATE_CHANGE,
+        data: getDateString(response.date, response.time),
+      });
+      MMSToggle();
+    };
+    mmsParsing();
+  };
   return (
     <InputListWrapper>
       <InputWrapper>
         <SmallArea>
-          <TextBox placeholder="메시지를 입력하세요" />
+          <TextBox placeholder="메시지를 입력하세요" value={mmsText} onChange={textChange} />
           <ButtonLayout>
-            <div>버튼1</div>
-            <div>버튼2</div>
+            <Buttons backgroundColor={color.MODAL_RED} onClick={MMSToggle}>
+              취소
+            </Buttons>
+            <Buttons backgroundColor={color.MINT} onClick={clickConfirm}>
+              변환
+            </Buttons>
           </ButtonLayout>
         </SmallArea>
       </InputWrapper>
