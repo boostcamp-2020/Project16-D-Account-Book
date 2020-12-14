f49bc46d842d5036417e2d3886c2b5171c57820a
bbbyung2 <bhko0524@naver.com>
2020-12-14 20:05:37 +0900

    feat: 결제수단 관리 페이지 로딩 시 Spinner 출력 기능 추가

--- client/src/store/AccountStore.tsx
+++ client/src/store/AccountStore.tsx
@@ -12,6 +12,9 @@ export default class AccountStore {
   @observable
   accountNames: string[] = [];
 
+  @observable
+  isLoading = true;
+
   constructor(rootStore: RootStore) {
     this.rootStore = rootStore;
     makeObservable(this);
@@ -31,6 +34,7 @@ export default class AccountStore {
     }
     const { value: refreshedValue } = yield generator.next();
     this.changeAccounts(refreshedValue);
+    this.isLoading = false;
   });
 
   createAccount = async (account: AccountRequest): Promise<void> => {
