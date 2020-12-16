9a0a5ca3e25f9c23b8716f7d094053b4a43f4a14
lacomaco <lacomaco122@gmail.com>
2020-12-16 16:18:23 +0900

    reactor : 더미데이터 추가
    - 다양한 가계부 추가
    - 다양한 지출 케이스 추가
    - 다양한 수입 케이스 추가
    - 더 많은 카테고리,결제수단 추가

--- server/src/db/seeders/2020120230-account.js
+++ server/src/db/seeders/2020120230-account.js
@@ -1,52 +1,8 @@
+const { createBulkAccountData } = require('../../utils/bulk-data/createAccount');
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('account', [
-      {
-        id: 1,
-        name: '삼성카드',
-        color: '#1E90FF',
-        accountbook_id: '1',
-        deleted_at: null,
-        created_at: '2020-11-23',
-        updated_at: '2020-11-23',
-      },
-      {
-        id: 2,
-        name: '국민카드',
-        color: '#0048BA',
-        accountbook_id: '1',
-        deleted_at: null,
-        created_at: '2020-11-24',
-        updated_at: '2020-11-24',
-      },
-      {
-        id: 3,
-        name: '농협카드',
-        color: '#B0BF1A',
-        accountbook_id: '1',
-        deleted_at: null,
-        created_at: '2020-11-25',
-        updated_at: '2020-11-25',
-      },
-      {
-        id: 4,
-        name: '우리카드',
-        color: '#7CB9E8',
-        accountbook_id: '1',
-        deleted_at: null,
-        created_at: '2020-11-26',
-        updated_at: '2020-11-26',
-      },
-      {
-        id: 5,
-        name: '신한카드',
-        color: '#C9FFE5',
-        accountbook_id: '1',
-        deleted_at: null,
-        created_at: '2020-11-27',
-        updated_at: '2020-11-27',
-      },
-    ]);
+    await queryInterface.bulkInsert('account', createBulkAccountData());
   },
 
   down: async (queryInterface, Sequelize) => {
