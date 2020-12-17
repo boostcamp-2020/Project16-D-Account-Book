acda0c01b24754dd68bc73bdc357771808ce18c2
Mong-Gu <yhw8258@naver.com>
2020-12-18 01:32:21 +0900

    refactor: 가계부 선택 페이지에서 본인의 provider 보여주도록 개선

--- client/src/pages/accountbook-selection-page/AccountbookSelectionPage.tsx
+++ client/src/pages/accountbook-selection-page/AccountbookSelectionPage.tsx
@@ -10,6 +10,8 @@ import AccountbookDeleteByAdminModal from '../../components/common/modals/accoun
 import FormModalCreateAccountbook from '../../components/common/modals/form-modal-accountbook/FormModalCreateAccountbook';
 import ProfileDropdown from '../../components/common/profile-dropdown/ProfileDropdown';
 import HeaderNavigationRightTopWrapper from '../../components/common/header-navigation/HeaderNavigationRightTop';
+import NaverLogo from '../../components/provider-logo/NaverLogo';
+import KakaoLogo from '../../components/provider-logo/KakaoLogo';
 
 const ViewWrapper = styled.div`
   font-family: 'Spoqa Han Sans';
@@ -58,6 +60,7 @@ const AccountbookSelectionPage: React.FC = () => {
     <>
       <HeaderNavigationRightTopWrapper>
         <HeaderWrapper>
+          {userStore.provider === 'naver' ? <NaverLogo /> : <KakaoLogo />}
           <EmailWrapper>{userStore.email}</EmailWrapper>
           <ProfileDropdown />
         </HeaderWrapper>
