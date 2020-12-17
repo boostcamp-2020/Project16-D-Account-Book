c8479b21cea4ee99e67e2ce1d3dec35771494463
Mong-Gu <yhw8258@naver.com>
2020-12-17 22:58:48 +0900

    refactor: sms/mms 파싱 유틸 함수 리팩토링
    - 기존에는 second와 mili second까지 나왔던 문제 해결
    - 코드 구조가 지저분해져서 모듈화 진행

--- client/src/utils/mms/mms.ts
+++ client/src/utils/mms/mms.ts
@@ -3,12 +3,15 @@ import Account from '../../types/account';
 export const getDateString = (date: string, time: string): string => {
   const [month, day] = getMonthDay(date);
   const [hour, minute] = getHourMinute(time);
-  const currentTime = new Date();
-  currentTime.setMonth(month - 1);
-  currentTime.setDate(day);
-  currentTime.setHours(hour);
-  currentTime.setMinutes(minute);
-  return currentTime.toISOString();
+  const tempDate = new Date(70, 1, 1, 0, 0);
+  const response = getISOString(tempDate, month, day, hour, minute);
+  return response;
+};
+
+const getNowYear = () => {
+  const now = new Date();
+  console.log(now.getFullYear());
+  return now.getFullYear();
 };
 
 const getMonthDay = (date: string): [month: number, day: number] => {
@@ -19,6 +22,15 @@ const getMonthDay = (date: string): [month: number, day: number] => {
 const getHourMinute = (time: string): [hour: number, minute: number] => {
   const stringHourMinute = time.split(':');
   return [parseInt(stringHourMinute[0]), parseInt(stringHourMinute[1])];
+};
+
+const getISOString = (date, month, day, hour, minute) => {
+  date.setFullYear(getNowYear());
+  date.setMonth(month - 1);
+  date.setDate(day);
+  date.setHours(hour);
+  date.setMinutes(minute);
+  return date.toISOString();
 };
 
 export const findAccountByName = (name: string, accounts: Account[]): Account | undefined => {
