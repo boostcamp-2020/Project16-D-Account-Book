0c096b98d5378d351fef3afe28f8f9dd04fcec50
bbbyung2 <bhko0524@naver.com>
2020-12-17 23:58:25 +0900

    feat: BlueButton 비활성화 시키는 인자로 disabled 추가
    - 비활성화 시 버튼 색상은 DISABELD_GRAY로 바꾸고 pointer-events는 none으로 설정

--- client/src/components/common/buttons/BlueButton.tsx
+++ client/src/components/common/buttons/BlueButton.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ModalCommonButton } from '../../../types/buttonTypes';
-import { BLUE } from '../../../constants/color';
+import { BLUE, WHITE, DISABLED_GRAY } from '../../../constants/color';
 
-const Wrapper = styled.div`
-  background-color: ${BLUE};
+const Wrapper = styled.div<{ disabled?: string }>`
+  background-color: ${({ disabled }) => (disabled ? DISABLED_GRAY : BLUE)};
   padding: 10px;
   border-radius: 5px;
   text-align: center;
-  color: white;
+  color: ${WHITE};
   cursor: pointer;
+  pointer-events: ${({ disabled }) => disabled};
 `;
 
 const BlueButton: React.FC<ModalCommonButton> = ({ children, onClick }: ModalCommonButton) => {
