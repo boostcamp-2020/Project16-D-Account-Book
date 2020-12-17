044537da090bb0ac7c76d30dda4255e9a2145f74
Mong-Gu <yhw8258@naver.com>
2020-12-18 01:24:08 +0900

    feat: 카카오 로고 스토리북 생성

---
+++ client/src/components/provider-logo/NaverLogo.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NaverLogo from './NaverLogo';
+
+export default {
+  title: 'Provider-Logo/Naver',
+};
+
+export const defaultLogo = (): JSX.Element => {
+  return <NaverLogo />;
+};
