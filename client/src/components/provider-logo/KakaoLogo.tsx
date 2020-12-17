ba0a6a3da4a0f7b8de8ee2e2e1254eb794d92534
Mong-Gu <yhw8258@naver.com>
2020-12-18 01:23:37 +0900

    feat: 카카오 로고 컴포넌트 생성

---
+++ client/src/components/provider-logo/KakaoLogo.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import kakaoLogo from '../../assets/images/kakao-abbreviation.png';
+
+const StyledDiv = styled.div`
+  img {
+    width: 1.3rem;
+    height: 1.3rem;
+    border-radius: 3px;
+  }
+`;
+
+const KakaoLogo: React.FC = () => {
+  return (
+    <StyledDiv>
+      <img src={String(kakaoLogo)} alt="kakao_logo" />
+    </StyledDiv>
+  );
+};
+
+export default KakaoLogo;
